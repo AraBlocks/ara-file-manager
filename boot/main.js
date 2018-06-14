9b326dced96cb017d162d9d8ebf1a63f01bed51b
Mark Romano <markgromano31@gmail.com>
2018-06-14 09:56:56 -0400

    feature(browser/views/home) added a simple home view
    -added a css tool, taken from littlstar.js

--- boot/main.js
+++ boot/main.js
@@ -1,8 +1,9 @@
 'use strict'
-
+const path = require('path')
 const isDev = require('electron-is-dev')
 require('../kernel/server.js')
-if(isDev) { require('electron-reload')(__dirname) }
+
+if (isDev) { require('electron-reload')(path.resolve('browser')) }
 require('./makeWindow')
 
 
