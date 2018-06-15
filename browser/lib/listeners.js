8c60e5dfd274ccd6827044bf59ee844701a21531
Mark Romano <markgromano31@gmail.com>
2018-06-15 10:41:31 -0400

    refactor(browser/views/confirm) now takes in load from socket

--- browser/lib/listeners.js
+++ browser/lib/listeners.js
@@ -5,8 +5,13 @@ const { ipcRenderer } = require('electron')
 
 module.exports = (state, emitter) => {
   ipcRenderer.once(kPurchaseRequest, (mainEmitter, load) => {
-    state.purchaseRequest = load
-    emitter.emit(state.events.PUSHSTATE, '/confirm')
+    const keys = Object.keys(load)
+    const valid = keys.includes('title') && keys.includes('description') && keys.includes('license')
+
+    if (valid) {
+      state.purchaseRequest = load
+      emitter.emit(state.events.PUSHSTATE, '/confirm')
+    }
   })
 
 }
