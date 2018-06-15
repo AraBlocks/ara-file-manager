4a41a71459934a3139582980df6233ee401929e4
Mark Romano <markgromano31@gmail.com>
2018-06-15 10:41:31 -0400

    refactor(browser/views/confirm) now takes in load from socket

--- test.js
+++ test.js
@@ -3,5 +3,10 @@ const Socket = require('simple-websocket')
 const socket = Socket('ws://localhost:24860')
 
 socket.on('connect', () => {
-  socket.send('!')
+  const item = JSON.stringify({
+    title: 'The Room',
+    license: 5,
+    description: 'The best movie ever!'
+  })
+  socket.send(item)
 })
