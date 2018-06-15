a92bf9136a1fe6ff4e395b9fe9e86a95aad8887c
Mark Romano <markgromano31@gmail.com>
2018-06-15 12:51:40 -0400

    feature(browser/views/confirm/purchasebutton) create a button component that linked to purchase method on ara3
    -refactored purchase method on ara3 to return a promise

--- browser/lib/store.js
+++ browser/lib/store.js
@@ -1,8 +1,12 @@
 'use strict'
 
+const Ara3 = require('../../lib/ethereum/ara3')
+const { privateKey, publicAddress } = require('../../lib/ethereum/account')
+
 module.exports = (app) => {
   app.use((state, emitter) => {
     state.purchaseRequest = {}
+    state.ara3 = new Ara3({ privateKey, publicAddress})
     require('./listeners')(state, emitter)
   })
 }
