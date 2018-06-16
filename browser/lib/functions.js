5554d0d999485c955d31f116faf568d66283caaa
Mark Romano <markgromano31@gmail.com>
2018-06-15 21:12:49 -0400

    chore(removed unused functions.js)

--- browser/lib/functions.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { kSendDataRequest } = require('../../lib/ipc')
-const { ipcRenderer: ipc } = require('electron')
-
-module.exports = {
-  getData: () => ipc.send(kSendDataRequest)
-}
