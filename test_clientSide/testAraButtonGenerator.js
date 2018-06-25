7fe892249ff609e1db024150361e728fd3ce65b3
ijl0322 <ijl0322@gmail.com>
2018-06-25 13:52:34 -0400

    test: add onclick test for araButtonGenerator

--- test_clientSide/testAraButtonGenerator.js
+++ test_clientSide/testAraButtonGenerator.js
@@ -1,26 +1,36 @@
  describe("Testing", function () {
+  let btn
   const araButtonClass = "ara-button"
   const araButtonTarget = "ara-buttons"
   const araButtonGenerator = new AraButtonGenerator("wss://echo.websocket.org")
+  const item = JSON.stringify({
+    title: 'The Room',
+    license: 5,
+    description: 'The best movie ever!',
+    price: 33
+  })
 
   before(function() {
-    const item = JSON.stringify({
-      title: 'The Room',
-      license: 5,
-      description: 'The best movie ever!',
-      price: 33
-    })
-    let btn = araButtonGenerator.makeButton(item, araButtonTarget, araButtonClass)
+    btn = araButtonGenerator.makeButton(item, araButtonTarget, araButtonClass)
     btn.innerHTML = "ARA"
-  });
+  })
 
-  it("should init one ara button", function () {
+  it("should init one ara button with correct class", function () {
     const buttons = document.getElementsByClassName(araButtonClass)
-    expect(buttons.length).to.be.equal(1);
-  });
+    expect(buttons.length).to.be.equal(1)
+  })
 
   it("should add one ara button to target", function () {
     const buttonsGroup = document.getElementsByClassName(araButtonTarget)
-    expect(buttonsGroup.length).to.be.equal(1);
+    expect(buttonsGroup.length).to.be.equal(1)
   })
-});
+
+  it("should send license on click", function (done) {
+    araButtonGenerator.onopen = () => { btn.click() }
+    araButtonGenerator.onmessage = function (evt) { 
+      const received_msg = evt.data
+      expect(evt.data).to.be.equal(item)
+      done()
+    }
+  })
+})
