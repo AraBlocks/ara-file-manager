259b91a38a890624299b2960aa9d957feb7f2c0d
Mark Romano <markgromano31@gmail.com>
2018-06-26 16:44:44 -0400

    feature(added develop view feature)

---
+++ browser/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const views = __dirname + '/views/'
+const remote = require('electron').remote;
+const windowManager = remote.require('electron-window-manager')
+const fs = require('fs')
+
+fs.readdirSync(views).forEach((view, i) => {
+  const handler = (e) => windowManager.bridge.emit(view)
+  windowManager.sharedData.set('current', view.slice(0, view.length - 3))
+
+  const button = document.createElement('button')
+  button.innerHTML = view
+  button.onclick = handler
+  document.body.appendChild(button)
+  const br = document.createElement('br')
+  document.body.appendChild(br)
+})
--- main.js
+++
@@ -1,8 +0,0 @@
-const electron = require('electron')
-const { app, BrowserWindow} = require('electron')
-const windowManager = require('electron-window-manager')
-
-app.on('ready', () => {
-  windowManager.init()
-  windowManager.open('home', 'Welcome','file://' + __dirname + '/index.html', false, { showDevTools: true })
-})
