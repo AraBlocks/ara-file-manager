1dcf43137b20335ccf41feb16397e77b60a88824
Mark Romano <markgromano31@gmail.com>
2018-07-02 13:10:03 -0400

    feature(): added archive function to registration process
    -added a method to registration store that attempts to archive newly generated id
    -archiver key hardcoded for now as `archiver`

--- browser/lib/store/registration.js
+++ browser/lib/store/registration.js
@@ -1,11 +1,20 @@
 const context = require('ara-context')()
 const aid = require('ara-identity')
-console.log(aid.util)
+const secrets = require('ara-network/secrets')
+
+const key = 'archiver'
 module.exports = {
-  async register(password) {
+  async createId(password) {
     const araId = await aid.create({ context, password })
     await aid.util.writeIdentity(araId)
     return araId
+  },
+
+  async archive(identity) {
+    const doc = await secrets.load({ key })
+
+    const { keystore } = doc.public || doc.secret
+    return await aid.archive(identity, { keystore, key })
   }
 }
 
