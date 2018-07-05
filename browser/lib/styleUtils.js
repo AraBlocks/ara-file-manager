fd0a9bcbb9b607e0642cda69fdc313645543be2c
Mark Romano <markgromano31@gmail.com>
2018-07-05 13:44:06 -0400

    feature(dynamic button component): added a button with dynamic attributes
    -attributes passed in as state
    -attributes intended to be changed, unlike other button component
    -refactored fileDescriptor to use this

--- browser/lib/styleUtils.js
+++ browser/lib/styleUtils.js
@@ -24,6 +24,9 @@ module.exports = {
       case 'blue':
         selectedColor = '#1e7dfa'
         break
+      case 'grey':
+        selectedColor = '#cbcbcb'
+        break
       default:
         selectedColor = '#fc2636'
     }
