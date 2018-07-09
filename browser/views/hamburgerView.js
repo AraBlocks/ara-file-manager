5636e5a1b8a81f7cf4edab4b237296a6950b4b57
Isabel Lee <ijl0322@users.noreply.github.com>
2018-07-09 14:22:39 -0400

    PE-116 feature: hamburger menu item (#32)
    * feature: hamburger menu item
    
    * refactor

---
+++ browser/views/hamburgerView.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const ManagerMenuItem= require('./managerMain/managerMenuItem')
+const html = require('choo/html')
+const Nanocomponent = require('nanocomponent')
+const { colors } = require('../lib/styleUtils')
+
+class HamburgerView extends Nanocomponent {
+  constructor() {
+    super()
+
+    this.hamburger = new ManagerMenuItem({
+      children: "File Manager", 
+      onclick: () => console.log("click")
+    })
+    window.hamburger = this
+  }
+
+  update() {
+    return true
+  }
+
+  createElement(chooState) {
+    const { hamburger } = this
+
+    return html`
+      <div class="popup">
+        <div style="
+          width: 115px;
+          height: 140px;
+          border: 1px solid #cbcbcb;
+        ">
+        ${hamburger.render()}
+        <div style="
+          width: 100%;
+          height: 1px;
+          background-color: ${colors.araGrey};
+        "></div>
+        ${hamburger.render()}
+        <div style="
+          width: 100%;
+          height: 1px;
+          background-color: ${colors.araGrey};
+        ">
+        </div>
+      </div>`
+  }
+}
+
+module.exports = HamburgerView
