a05781dd67a2f27a050dde375659f77519bb55fa
Isabel Lee <ijl0322@users.noreply.github.com>
2018-07-09 17:28:15 -0400

    feature: PE-116 hamburger menu (#33)

--- browser/components/hamburgerMenu/styles/menuItem.js
+++ browser/components/hamburgerMenu/styles/menuItem.js
@@ -10,7 +10,7 @@ module.exports = {
   container: css`
     :host {
       align-items: center;
-      display: flex;
+      display: flex;   
       font-family: ${fonts.light};
       font-size: 16px;
       padding-left: 10px;
