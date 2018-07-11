9b05628667a10b3b1345f15c6c6074f9ac41f198
mark g romano <markgromano31@gmail.com>
2018-07-11 14:39:13 -0400

    Pe 125 mr file manager container component (#43)
    * rebase
    
    * feature(tabItem component) reusable component for making tab menus
    
    * feature(container component) container that holds all filemanager components
    
    * rebase
    
    * feature(tabItem component) reusable component for making tab menus
    
    * feature(container component) container that holds all filemanager components

--- browser/components/styles/tabItem.js
+++ browser/components/styles/tabItem.js
@@ -8,7 +8,7 @@ module.exports = {
     return css`
       :host {
         color: ${isActive ? colors.araRed : 'black'};
-        font-family: ${isActive ? fonts.bold : fonts.regular};
+        font-family: ${fonts.regular};
         font-size: 14px;
       }
     `
