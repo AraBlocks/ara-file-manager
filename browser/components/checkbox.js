1440ae74716be392a23cc299e9794c2cdecae48f
ijl0322 <ijl0322@gmail.com>
2018-07-12 17:31:51 -0400

    feature: add checkbox

---
+++ browser/components/checkbox.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const styles = require('./styles/checkbox')
+const html = require('choo/html')
+const Nanocomponent = require('nanocomponent')
+
+class Checkbox extends Nanocomponent {
+	constructor({ 
+		checked, 
+		cssClass = {} 
+	}) {
+		super()
+		this.props = { cssClass }
+		this.state = { checked }
+		this.onclick = this.onclick.bind(this)
+	}
+
+	onclick() {
+		this.state.checked = !this.state.checked
+		this.rerender()
+	}
+
+	update({ checked }) {
+		const { state } = this
+		const sameState = state.checked == checked
+		if (!sameState) {
+			state.checked = checked
+		}
+		return !sameState
+	}
+
+	createElement() {
+		const { onclick, props, state } = this
+		return html`
+			<div 
+				class=${styles.container({	
+					checked: state.checked, 
+					opts: (props.cssClass.opts || {})
+				})} 
+				onclick=${onclick}
+			>
+				<div class=${styles.checkmark({
+					checked: state.checked,
+					opts: (props.cssClass.opts || {})
+				})}
+				>
+					✓
+				</div>
+			</div>
+		`
+	}
+}
+
+module.exports = Checkbox
