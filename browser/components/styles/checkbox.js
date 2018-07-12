b2b9cdf02ef9ae168c604f59e5028e0fdce5d023
ijl0322 <ijl0322@gmail.com>
2018-07-12 17:31:51 -0400

    feature: add checkbox

---
+++ browser/components/styles/checkbox.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { 
+	colors,
+	colorSelector, 
+	fonts } = require('styleUtils')
+const { css } = require('css')
+
+module.exports = {
+	colors,
+	fonts,
+
+	container({ checked, 
+		opts: { 
+			colorChecked = 'red',
+			colorUnchecked = 'grey'
+		}
+	}) {
+		console.log(colorChecked)
+		return css`
+			:host {
+				width: 50px;
+				height: 50px;
+				background-color: white;
+				border: 2px solid ${checked 
+					? colorSelector(colorChecked) 
+					: colorSelector(colorUnchecked)
+				};
+			}
+		`
+	},
+
+	checkmark({ 
+		checked, 
+		opts: { colorChecked = 'red' }
+	}) {
+		return css`
+			:host {
+				font-size: 40px;
+				color: ${colorChecked};
+				text-align: center;
+				display: ${checked ? "block" : "none"};
+			}
+		`
+	}
+}
