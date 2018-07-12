662ccdbf3185133075f25823f0245e36e695d85a
ijl0322 <ijl0322@gmail.com>
2018-07-12 17:31:51 -0400

    feature: add checkbox

---
+++ browser/views/checkboxView.js
@@ -0,0 +1,36 @@
+'use strict'
+
+//const styles = require('./styles/tooltip')
+const Checkbox = require('../components/checkbox')
+const html = require('choo/html')
+const Nanocomponent = require('nanocomponent')
+
+class CheckboxView extends Nanocomponent {
+	constructor() {
+		super()
+		this.props = {}
+		this.checkbox = new Checkbox({ 
+			checked: false, 
+			cssClass: {
+				opts: {
+					colorChecked: 'blue'
+				}
+			}
+		})
+	}
+
+	update() {
+		return true	
+	}
+
+	createElement() {
+		const { checkbox, props } = this
+		return html`
+			<div>
+				${checkbox.render()}
+			</div>
+		`
+	}
+}
+
+module.exports = CheckboxView
