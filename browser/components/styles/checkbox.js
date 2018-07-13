3255591b1566fbfc6560687d2983945bf8e1b5b3
Isabel Lee <ijl0322@users.noreply.github.com>
2018-07-13 16:56:27 -0400

    feature: PE-142 checkbox (#48)

---
+++ browser/components/styles/checkbox.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const { 
+	colors,
+	colorSelector, 
+	fonts 
+} = require('styleUtils')
+const { css } = require('css')
+
+module.exports = {
+	colors,
+	fonts,
+
+	container({ 
+		checked, 
+		opts: { 
+			colorChecked = 'red',
+			colorUnchecked = 'grey'
+		}
+	}) {
+		return css`
+			:host {
+				width: 50px;
+				height: 50px;
+				background-color: white;
+				border: 2px solid ${
+					checked 
+					? colorSelector(colorChecked) 
+					: colorSelector(colorUnchecked)
+				};
+				cursor: pointer;
+			}
+		`
+	},
+
+	checkmark({ 
+		checked, 
+		opts: { colorChecked = 'red' }
+	}) {
+		return css`
+			:host {
+				font-size: 40px;
+				color: ${colorChecked};
+				text-align: center;
+				display: ${checked ? "block" : "none"};
+			}
+		`
+	}
+}
