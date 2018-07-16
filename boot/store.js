720db205cc191327da5075ec97222d898f91af26
mark g romano <markgromano31@gmail.com>
2018-07-16 14:33:27 -0400

    Pe 148 mr global store (#55)
    * cache
    
    * feature(store): added a basic global store

---
+++ boot/store.js
@@ -0,0 +1,56 @@
+'use strict'
+
+module.exports = {
+  userData: {
+    username: 'cryptokitter',
+    userBalance: 135.23
+  },
+  files: {
+    published: mockFiles(),
+    purchased: mockFiles()
+  }
+}
+
+function mockFiles() {
+  return [
+  {
+      downloadPercent: 0,
+      meta: {
+        aid: 'did:ara:c7b86c29be073c0ceb27da22c03f10e7fadb9eb32dcf4a362639993cf963e6a6',
+        datePublished: '11/20/1989',
+        earnings: 237.43,
+        peers: 1003,
+        price: 56.99,
+      },
+      name: 'Adobe Photoshop',
+      size: 10.67,
+      status: 0,
+    },
+    {
+      downloadPercent: 1,
+      meta: {
+        aid: 'did:ara:c7b86c29be073c0ceb27da22c03f10e7fadb9eb32dcf4a362639993cf963e6a6',
+        datePublished: '11/20/1989',
+        earnings: 54.33,
+        peers: 33,
+        price: 10.99,
+      },
+      name: 'Microsoft Word',
+      size: 4.67,
+      status: 2,
+    },
+    {
+      downloadPercent: 0,
+      meta: {
+        aid: 'did:ara:c7b86c29be073c0ceb27da22c03f10e7fadb9eb32dcf4a362639993cf963e6a6',
+        datePublished: '11/20/1989',
+        earnings: 2134.33,
+        peers: 353,
+        price: 3.99,
+      },
+      name: 'Microsoft PowerPoint',
+      size: 1.67,
+      status: 0,
+    }
+  ]
+}
