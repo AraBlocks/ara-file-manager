2e7f817cff70415de6de010224d3cd0335a05e76
Mark Romano <markgromano31@gmail.com>
2018-07-16 13:49:51 -0400

    cache

--- boot/main-dev.js
+++ boot/main-dev.js
@@ -8,8 +8,9 @@ require('./ipc-dev')
 
 app.on('ready', () => {
   windowManager.init()
+  windowManager.sharedData.set('store', require('./store'))
   if (isDev) { require('electron-reload')(path.resolve('browser')) }
   require('./tray')()
 })
 
-app.on('window-all-closed',() =>{})
+app.on('window-all-closed', () =>{})
