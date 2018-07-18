cd4d823b1f0121130f7c6b0fd8908547e97c817d
mark g romano <markgromano31@gmail.com>
2018-07-18 18:27:50 -0400

    Pe 192 mr dynamic button testing (#67)
    * cache
    
    * testing implemented
    
    * feature(dynamic button test)
    
    * feature(dynamic button test)
    
    * chore(removed dead code)

---
+++ test/components/dynamicButton.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const DynamicButton = require('../../browser/components/dynamicButton')
+
+describe("DynamicButton Component", () => {
+  it('Should update state', () => {
+    const updatedState = {
+      children: 'Updated',
+      onclick: () => {},
+      cssClass: {},
+      onmouseout: () => {},
+      onmouseover: () => {},
+      type: 'New Type'
+    }
+    const dynamicButton = new DynamicButton({})
+    const initialState = dynamicButton.state
+
+    dynamicButton.render(updatedState)
+
+    Object.entries(initialState).forEach(([attr]) => {
+      expect(initialState[attr]).not.to.equal(updatedState[attr])
+    })
+  })
+})
