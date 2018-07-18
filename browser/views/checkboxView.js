84b337667e23c90f03275a5a10936d8756354439
Isabel Lee <ijl0322@users.noreply.github.com>
2018-07-18 15:55:50 -0400

    feature: add currency converter (#64)

--- browser/views/checkboxView.js
+++ browser/views/checkboxView.js
@@ -2,6 +2,7 @@
 
 const Checkbox = require('../components/checkbox')
 const FileSlector = require('../components/fileSelector')
+const Input = require('../components/input')
 const OptionsCheckbox = require('../components/optionsCheckbox')
 const html = require('choo/html')
 const Nanocomponent = require('nanocomponent')
@@ -9,7 +10,12 @@ const Nanocomponent = require('nanocomponent')
 class CheckboxView extends Nanocomponent {
 	constructor() {
 		super()
-		this.state = { checkbox: false, supernode: false, filePath: '' }
+		this.state = {
+			checkbox: false,
+			supernode: false,
+			filePath: '' ,
+			currency: ''
+		}
 		this.checkbox = new Checkbox({
 			checked: false,
 			cssClass: {
@@ -32,6 +38,21 @@ class CheckboxView extends Nanocomponent {
 			field: 'filePath',
 			parentState: this.state
 		})
+
+		this.currencySelector = new Input({
+			field: 'price',
+			parentState: this.state,
+			placeholder: 'Price',
+			type: 'number',
+      embeddedButton: {
+        option: 'selection',
+        optionList: [
+          'ARA',
+          'USD'
+				],
+				field: 'currency'
+      }
+		})
 	}
 
 	update() {
@@ -39,13 +60,20 @@ class CheckboxView extends Nanocomponent {
 	}
 
 	createElement() {
-		const { checkbox, optionsCheckbox, fileSelector, state } = this
+		const {
+			checkbox,
+			currencySelector,
+			optionsCheckbox,
+			fileSelector,
+			state
+		} = this
 		return html`
 			<div>
 				${checkbox.render()}
 				${optionsCheckbox.render()}
 				<button onclick=${onclick}>log all states</button>
 				${fileSelector.render()}
+				${currencySelector.render()}
 			</div>
 		`
 
