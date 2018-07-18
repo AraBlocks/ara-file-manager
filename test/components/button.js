4dfda28acf69d856beb7caeb6dd2083b92750826
Mark Romano <markgromano31@gmail.com>
2018-07-18 18:08:25 -0400

    feature(dynamic button test)

--- test/components/button.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const Button = require('../../browser/components/button')
-const { expect } = require('chai')
-
-it("should pass", () => {
-  const button = new Button({})
-  expect(0).to.be.equal(0)
-})
