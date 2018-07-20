4f8499fcf70b3615cf6ad01a842155df65046aff
ijl0322 <ijl0322@gmail.com>
2018-07-20 14:25:14 -0400

    style: minor fixes

--- test/components/fileSelector.js
+++ test/components/fileSelector.js
@@ -6,8 +6,8 @@ describe('FileSelector Component', () => {
 	it('Should Update State', () => {
 		const updatedState = { filePath: 'newFilePath' }
 		const fileSelector = new FileSelector({})
-
 		const initialState = fileSelector.state.filePath
+
 		fileSelector.render(updatedState)
 		expect(fileSelector.state.filePath !== initialState)
 	})
