d60f52733fe117625d0c8c6e3a2ee3645e6b3a75
mark g romano <markgromano31@gmail.com>
2018-07-20 11:11:21 -0400

    feature(tabitem tests) (#73)
    * feature(tabitem tests)
    
    * chore(rebased)
    
    * chore(rebased)

--- test/components/tabItem.js
+++ test/components/tabItem.js
@@ -2,4 +2,11 @@
 
 const TabItem = require('../../browser/components/tabItem')
 
-describe('TabItem')
+describe('TabItem', () => {
+  let tabItem
+  beforeEach(() => tabItem = new TabItem({ isActive: true }))
+
+  it('Should work 🤷‍', () => {
+    expect(true).to.equal(true)
+  })
+})
