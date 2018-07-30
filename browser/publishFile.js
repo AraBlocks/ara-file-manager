5a0087868213e93822bfd43fb17219fee7a31c51
Mark Romano <markgromano31@gmail.com>
2018-07-30 15:07:03 -0400

    refactor(): moved from bridge to ipcRenderer

--- browser/publishFile.js
+++ browser/publishFile.js
@@ -7,13 +7,13 @@ const {
   PUBLISHED,
 } = require('../lib/constants/stateManagement')
 const PublishFile = require('./views/publishFile/container')
-const { remote } = require('electron')
+const { ipcRenderer, remote } = require('electron')
 const windowManager = remote.require('electron-window-manager')
 const { account } = windowManager.sharedData.fetch('store')
 
 const publishFile = new PublishFile({ account })
 document.getElementById('container').appendChild(publishFile.render())
 
-windowManager.bridge.on(ESTIMATING_COST, () => publishFile.render(true))
-windowManager.bridge.on(ESTIMATION, () => openModal('publishConfirmModal'))
-windowManager.bridge.on(PUBLISHED, closeWindow)
+ipcRenderer.on(ESTIMATING_COST, () => publishFile.render(true))
+ipcRenderer.on(ESTIMATION, () => openModal('publishConfirmModal'))
+ipcRenderer.on(PUBLISHED, () => closeWindow.apply(this))
