e5ed20e5292f524894f0087f216ae73831617ee6
Isabel Lee <ijl0322@users.noreply.github.com>
2018-07-31 11:49:22 -0400

    refactor: clean up link download files (#96)

--- browser/lib/download.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const { DOWNLOAD } = require('../../lib/constants/stateManagement')
-const { remote } = require('electron')
-const windowManager = remote.require('electron-window-manager')
-
-module.exports = () => windowManager.bridge.emit(DOWNLOAD, windowManager.fileInfo.aid)
--- kernel/lib/actionCreators/download.js
+++ kernel/lib/actionCreators/download.js
@@ -3,51 +3,32 @@
 const dispatch = require('../reducers/dispatch')
 const { download } = require('../actions')
 const { DOWNLOAD, DOWNLOADED, DOWNLOADING } = require('../../../lib/constants/stateManagement')
+const { ipcMain } = require('electron')
 const windowManager = require('electron-window-manager')
 
-windowManager.bridge.on(DOWNLOAD, async (load) => {
-	windowManager.bridge.emit(DOWNLOADING)
+ipcMain.on(DOWNLOAD, async (event, load) => {
+	windowManager.get('filemanager').object.webContents.send(DOWNLOADING)
 	const newState = dispatch({
     type: DOWNLOADING,
     load: {
-      purchased: [
-				{
-					downloadPercent: 0,
-					meta: {
-						aid: windowManager.fileInfo.aid,
-						datePublished: '11/20/1989',
-						earnings: 2134.33,
-						peers: 353,
-						price: windowManager.fileInfo.price,
-					},
-					name: windowManager.fileInfo.fileName,
-					size: 1.67,
-					status: 1,
-				}
-			]
-    }
-  })
-	download({did: load, handler: () => {
-		windowManager.bridge.emit(DOWNLOADED)
-		const newState = dispatch({
-			type: DOWNLOADING,
-			load: {
-				purchased: [
-					{
-						downloadPercent: 0,
-						meta: {
-							aid: windowManager.fileInfo.aid,
-							datePublished: '11/20/1989',
-							earnings: 2134.33,
-							peers: 353,
-							price: windowManager.fileInfo.price,
-						},
-						name: windowManager.fileInfo.fileName,
-						size: 1.67,
-						status: 2,
-					}
-				]
+				downloadPercent: 0,
+				meta: {
+					aid: windowManager.fileInfo.aid,
+					datePublished: '11/20/1989',
+					earnings: 2134.33,
+					peers: 353,
+					price: windowManager.fileInfo.price,
+				},
+				name: windowManager.fileInfo.fileName,
+				size: 1.67,
+				status: 1,
 			}
+	})
+	download({did: windowManager.fileInfo.aid, handler: () => {
+		const newState = dispatch({
+			type: DOWNLOADED,
+			load: windowManager.fileInfo.aid
 		})
+		windowManager.get('filemanager').object.webContents.send(DOWNLOADED)
 	}})
 })
