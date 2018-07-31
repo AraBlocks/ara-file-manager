8a51fbe973f8d2938be02628a72195d70dd6ae10
ijl0322 <ijl0322@gmail.com>
2018-07-31 15:49:18 -0400

    feature: resolve afs path for downloading

--- kernel/lib/actions/afsManager.js
+++ kernel/lib/actions/afsManager.js
@@ -1,83 +1,104 @@
 const { create } = require('ara-filesystem')
+const { createAFSKeyPath } = require('ara-filesystem/key-path')
 const { createSwarm } = require('ara-network/discovery')
+const fs = require('fs')
+const path = require('path')
 
-// broadcast('did:ara:acd9cb17d7b89bda1471f699d95273bfba9150b0237c044ac0f4e83de19fe3c2')
-async function broadcast (did) {
-    // Create a swarm for uploading the content
-    const { afs } = await create({did})
+async function broadcast(did) {
+	// Create a swarm for uploading the content
+	const { afs } = await create({ did })
 
-    // Join the discovery swarm for the requested content
-    const opts = {
-        stream: stream,
-    }
-    const swarm = createSwarm(opts)
-    swarm.on('connection', handleConnection)
-    swarm.join(did)
+	// Join the discovery swarm for the requested content
+	const opts = {
+		stream: stream,
+	}
+	const swarm = createSwarm(opts)
+	swarm.on('connection', handleConnection)
+	swarm.join(did)
 
-   function stream(peer) {
-       const stream = afs.replicate({
-            upload: true,
-            download: false
-       })
-       stream.once('end', onend)
-       stream.peer = peer
-       return stream
-   }
+	function stream(peer) {
+		const stream = afs.replicate({
+			upload: true,
+			download: false
+		})
+		stream.once('end', onend)
+		stream.peer = peer
+		return stream
+	}
 
-   async function onend(){
-        console.log(`Uploaded!`)
-   }
+	async function onend() {
+		console.log(`Uploaded!`)
+	}
 
-    async function handleConnection(connection, info){
-        console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
-    }
+	async function handleConnection(connection, info) {
+		console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
+	}
 }
 
-async function download ({did, handler}) {
+async function download({ did, handler }) {
 	console.log('Creating afs...')
 	// Create a swarm for downloading the content
-	const { afs } = await create({did})
+	const { afs } = await create({ did })
 
 	// Join the discovery swarm for the requested content
 	console.log('Waiting for peer connection...')
-    const opts = {
-        stream: stream,
-    }
-    const swarm = createSwarm(opts)
-    swarm.on('connection', handleConnection)
-    swarm.join(did)
+	const opts = {
+		stream: stream,
+	}
+	const swarm = createSwarm(opts)
+	swarm.once('connection', handleConnection)
+	swarm.join(did)
 
-   function stream(peer) {
-       const stream = afs.replicate({
-            upload: false,
-            download: true
-       })
-       stream.once('end', onend)
-       stream.peer = peer
-       return stream
-   }
+	function stream(peer) {
+		const stream = afs.replicate({
+			upload: false,
+			download: true
+		})
+		stream.once('end', onend)
+		stream.peer = peer
+		return stream
+	}
 
-   async function onend(){
-       console.log(await afs.readdir('.'))
-       console.log(`Downloaded!`)
-       afs.close()
-       swarm.destroy()
-       handler()
-        console.log("Swarm destroyed")
-    }
+	async function onend() {
+		const files = await afs.readdir('.')
+		renameAfsFiles(did, files[0])
+		console.log(files)
+		console.log(`Downloaded!`)
+		afs.close()
+		swarm.destroy()
+		handler()
+		console.log("Swarm destroyed")
+	}
 
-    async function handleConnection(connection, info){
-        console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
-        try {
-            await afs.download('.')
-        }
-        catch (err) {
-            console.log(`Error: ${err}`)
-        }
-    }
+	async function handleConnection(connection, info) {
+		console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
+		try {
+			await afs.download('.')
+		}
+		catch (err) {
+			console.log(`Error: ${err}`)
+		}
+	}
+}
+
+function getAfsPath(aid) {
+	const aidHash = aid.slice(8)
+	return path.join(createAFSKeyPath(aidHash), 'home', 'content')
+}
+
+function renameAfsFiles(aid, fileName) {
+	const afsFolderPath = getAfsPath(aid)
+	const afsFilePath = path.join(afsFolderPath, 'data')
+	const newPath = path.join(afsFolderPath, fileName)
+	fs.rename(afsFilePath, newPath, function(err) {
+		if (err) {
+			console.log('some error occurred')
+		}
+	})
 }
 
 module.exports = {
 	broadcast,
-	download
+	download,
+	getAfsPath
 }
