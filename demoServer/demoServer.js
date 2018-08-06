ccf9075ce779e18001a23842fdbea04ee44c63c3
ijl0322 <ijl0322@gmail.com>
2018-08-06 12:21:44 -0400

    feature: ping server without sending a html page

--- demoServer/demoServer.js
+++ demoServer/demoServer.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const http = require('http');
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/download/:id/:name/:price/', (req, res) => {
-	res.sendFile(__dirname + '/public'+ '/index.html')
+	const item = JSON.stringify({
+		fileName: req.params.name,
+		aid: req.params.id,
+		price: req.params.price
+	})
+
+	var post_options = {
+			host: 'localhost',
+			port: '3002',
+			path: '/',
+			method: 'POST',
+			headers: {
+					'Content-Type': 'application/json',
+					'Content-Length': Buffer.byteLength(item),
+			},
+			withCredentials: true
+	};
+
+	var post_req = http.request(post_options, function(res) {});
+	post_req.write(item);
+	post_req.end();
 })
 app.listen(3001, () => console.log('Demo server listening on port 3001!'))
