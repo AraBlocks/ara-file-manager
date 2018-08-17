12db3e653fea046b87a42db3f220cb54e3733f86
ijl0322 <ijl0322@gmail.com>
2018-08-17 12:29:20 -0400

    feature: deeplinking

--- boot/main.js
+++ boot/main.js
@@ -6,6 +6,18 @@ const isDev = require('electron-is-dev')
 const path = require('path')
 if (isDev) { require('./ipc-dev') }
 
+let deeplinkingUrl
+
+const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
+  if (process.platform == 'win32') {
+    deeplinkingUrl = argv.slice(1)
+  }
+})
+if (shouldQuit) {
+    app.quit()
+    return
+}
+
 app.on('ready', () => {
   windowManager.init()
   require('../kernel/lib/actionCreators')
@@ -16,5 +28,50 @@ app.on('ready', () => {
   globalShortcut.register('CommandOrControl+/', () => {
     windowManager.getCurrent().object.openDevTools()
   })
+  if (process.platform == 'win32') {
+    deeplinkingUrl = process.argv.slice(1)
+  }
+  if (deeplinkingUrl) {
+    openDeepLinking()
+  }
 })
 app.on('window-all-closed', () => {})
+
+// For Deek Linking
+app.setAsDefaultProtocolClient('lstr')
+app.on('open-url', function (event, url) {
+  event.preventDefault()
+  deeplinkingUrl = url
+  if (app.isReady()) {
+    openDeepLinking()
+  }
+})
+
+function openDeepLinking() {
+  const modalName = 'reDownloadModal'
+  if (windowManager.get(modalName).object != null) { return }
+  windowManager.sharedData.set('current', modalName)
+  windowManager.createNew(
+    modalName,
+    modalName,
+    windowManager.loadURL(modalName),
+    false,
+    {
+      backgroundColor: 'white',
+      frame: false,
+      ...windowManager.setSize(modalName),
+    }
+  ).open()
+  parseLink()
+}
+
+function parseLink() {
+  const linkElements = deeplinkingUrl.slice(7).split("/")
+  if (linkElements.length === 3 && linkElements[0] == 'download') {
+    windowManager.fileInfo = {
+      aid: linkElements[1],
+      fileName: linkElements[2],
+      price: 44
+    }
+  }
+}
