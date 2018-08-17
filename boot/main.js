ac5d61f9c9b5c4ce657ba9cd14973d7ecb590414
ijl0322 <ijl0322@gmail.com>
2018-08-17 15:39:21 -0400

    refactor: move openDeekLink to lsWindowManager

--- boot/main.js
+++ boot/main.js
@@ -1,17 +1,16 @@
 'use strict'
 
 const { app, globalShortcut } = require('electron')
-const { getAFSPrice } = require('../kernel/lib/actions/afsManager')
 const windowManager = require('../kernel/lib/lsWindowManager')
 const isDev = require('electron-is-dev')
 const path = require('path')
 if (isDev) { require('./ipc-dev') }
 
-let deeplinkingUrl
+let deepLinkingUrl
 
 const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
   if (process.platform == 'win32') {
-    deeplinkingUrl = argv.slice(1)
+    deepLinkingUrl = argv.slice(1)
   }
 })
 if (shouldQuit) {
@@ -24,16 +23,14 @@ app.on('ready', () => {
   require('../kernel/lib/actionCreators')
   if (isDev) { require('electron-reload')(path.resolve('browser')) }
   require('./tray')()
-  require('./server')()
-  require('../demoServer/demoServer')
   globalShortcut.register('CommandOrControl+/', () => {
     windowManager.getCurrent().object.openDevTools()
   })
   if (process.platform == 'win32') {
-    deeplinkingUrl = process.argv.slice(1)
+    deepLinkingUrl = process.argv.slice(1)
   }
-  if (deeplinkingUrl) {
-    openDeepLinking()
+  if (deepLinkingUrl) {
+    windowManager.openDeepLinking(deepLinkingUrl)
   }
 })
 app.on('window-all-closed', () => {})
@@ -42,39 +39,8 @@ app.on('window-all-closed', () => {})
 app.setAsDefaultProtocolClient('lstr')
 app.on('open-url', function (event, url) {
   event.preventDefault()
-  deeplinkingUrl = url
+  deepLinkingUrl = url
   if (app.isReady()) {
-    openDeepLinking()
+    windowManager.openDeepLinking(url)
   }
 })
-
-function openDeepLinking() {
-  parseLink()
-  getAFSPrice({ did: windowManager.fileInfo.aid, password: 'abc' }).then((price) => {
-    const modalName = 'reDownloadModal'
-    if (windowManager.get(modalName).object != null) { return }
-    windowManager.sharedData.set('current', modalName)
-    windowManager.createNew(
-      modalName,
-      modalName,
-      windowManager.loadURL(modalName),
-      false,
-      {
-        backgroundColor: 'white',
-        frame: false,
-        ...windowManager.setSize(modalName),
-      }
-    ).open()
-    windowManager.fileInfo.price = price
-  }).catch(console.log)
-}
-
-function parseLink() {
-  const linkElements = deeplinkingUrl.slice(7).split("/")
-  if (linkElements.length === 3 && linkElements[0] == 'download') {
-    windowManager.fileInfo = {
-      aid: linkElements[1],
-      fileName: linkElements[2],
-    }
-  }
-}
