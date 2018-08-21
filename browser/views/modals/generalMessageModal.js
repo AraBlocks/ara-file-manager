bfa2058c9b5d5fba1ae5abf9d1bfe4496cef986b
Isabel Lee <ijl0322@users.noreply.github.com>
2018-08-21 15:47:57 -0400

    feature: add general message modal (#135)
    PE-168 Success Modal
    PE-170 Not Enough Ara Modal
    PE-172 Disk Space Modal
    PE-174 File Missing Modal
    PE-175 Wallet Empty Modal
    PE-183 Delete Success Modal
    PE-184 Failure Modal 2
    PE-185 Not Enough Ara Modal 2

--- browser/views/modals/generalMessageModal.js
+++ browser/views/modals/generalMessageModal.js
@@ -1,23 +1,24 @@
 'use strict'
 
-const html = require('choo/html')
-const styles = require('./styles')
 const Button = require('../../components/button')
 const { closeWindow } = require('../../lib/tools/windowManagement')
+const generalModalTextProvider = require('../../lib/tools/generalModalTextProvider')
+const html = require('choo/html')
+const styles = require('./styles')
 
-module.exports = () => {
+module.exports = ({ modalName = 'fileMissing', fileName = 'Grump Cat' }) => {
   const confirmButton = new Button({
 		children: 'Confirm',
 		onclick: () => closeWindow()
 	})
+	const { description, title } = generalModalTextProvider(modalName, fileName)
   return html`
     <div class="${styles.container} modals-container">
       <div class="${styles.messageBold} modal-messageBold">
-				Something went wrong
+				${title}
       </div>
 			<div class="${styles.smallMessage} modal-smallMessage">
-				This transaction could not be completed.
-				It could be that the link is no longer valid, or that there are no peers sharing this file right now.
+				${description}
 			</div>
       <div>
         ${confirmButton.render()}
