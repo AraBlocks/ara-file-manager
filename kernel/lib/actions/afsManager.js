8e9a523e6170bd0930a7b0918778b00d9ec930fd
Isabel Lee <ijl0322@users.noreply.github.com>
2018-08-24 11:45:35 -0400

    feature: PE-388 DCDN (#142)

--- kernel/lib/actions/afsManager.js
+++ kernel/lib/actions/afsManager.js
@@ -1,46 +1,31 @@
 'use strict'
 
-const { create, getPrice, unarchive } = require('ara-filesystem')
+const { getPrice, unarchive } = require('ara-filesystem')
 const { createAFSKeyPath } = require('ara-filesystem/key-path')
-const { createSwarm } = require('ara-network/discovery')
-const fs = require('fs')
+const araNetworkNodeDcdn = require('ara-network-node-dcdn')
+const { publishDID } = require('ara-network-node-dcdn/subnet')
 const path = require('path')
+const windowManager = require('electron-window-manager')
 
 async function broadcast(did, handler) {
-	// Create a swarm for uploading the content
 	const fullDid = 'did:ara:' + did
-	console.log('broadcasting for ', fullDid)
-	const { afs } = await create({ did: fullDid }).catch((e) => {
-		console.log('Error creating afs when broadcasting', e)
-	})
-
-	// Join the discovery swarm for the requested content
-	const opts = {
-		stream: stream,
-	}
-	console.log('Creating swarm...')
-	const swarm = createSwarm(opts)
-	swarm.once('connection', handleConnection)
-	swarm.join(fullDid)
-	console.log('Joined Swarm')
-
-	function stream(peer) {
-		const stream = afs.replicate({
-			upload: true,
+	console.log(`broadcasting..${fullDid}`)
+	const { account: { aid } } = windowManager.sharedData.fetch('store')
+	try {
+		araNetworkNodeDcdn.start({
+			did: fullDid,
 			download: false,
+			upload: true,
 		})
-		stream.once('end', onend)
-		stream.peer = peer
-		return stream
-	}
 
-	async function onend() {
-		console.log(`Uploaded!`)
-	}
-
-	async function handleConnection(connection, info) {
-		handler()
-		console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
+		publishDID(did, {
+      identity: aid,
+      secret: Buffer.from('ara-archiver'),
+      name: 'remote1',
+      keys: path.resolve(`/Users/${process.argv[process.argv.length - 1]}/.ara/secret/ara-archiver.pub`),
+    })
+	} catch(e) {
+		console.log(e)
 	}
 }
 
@@ -50,75 +35,14 @@ async function getAFSPrice({ did, password }) {
 }
 
 async function download({ did, handler, errorHandler }) {
-	console.log('Creating afs...')
-	// Create a swarm for downloading the content
 	const fullDid = 'did:ara:' + did
-	const { afs } = await create({ did: fullDid }).catch(((err) => {
-		console.log({createErr: err})
-		errorHandler()
-	}))
-
-	afs.on('content', () => {
-		const feed = afs.partitions.resolve(afs.HOME).content
-		let prevPercent = 0
-		feed.on('download', () => {
-			const size = feed.byteLength
-			const total = feed.length
-			if (total) {
-				const downloaded = feed.downloaded()
-				const perc = downloaded / total
-				console.log({perc})
-				if (perc >= prevPercent + 0.04) {
-					prevPercent = perc
-					handler({ perc, size })
-				}
-			}
-		})
-		feed.on('sync', onend)
-	})
-
-	// Join the discovery swarm for the requested content
-	console.log('Waiting for peer connection...')
-	const opts = {
-		stream: stream,
-	}
-	const swarm = createSwarm(opts)
-	swarm.once('connection', handleConnection)
-	swarm.join(fullDid)
-
-	function stream(peer) {
-		const stream = afs.replicate({
-			upload: false,
+	try {
+		araNetworkNodeDcdn.start({
+			did: fullDid,
 			download: true
 		})
-		stream.once('end', onend)
-		stream.peer = peer
-		return stream
-	}
-
-	async function onend() {
-		const files = await afs.readdir('.').catch(err => {
-			console.log({readdirErr: err})
-			errorHandler()
-		})
-		unarchiveAFS({ did, path: makeAfsPath(did) })
-		console.log({files})
-		console.log(`Downloaded!`)
-		afs.close()
-		swarm.destroy()
-		handler({ percentage: 1 })
-		console.log("Swarm destroyed")
-	}
-
-	async function handleConnection(connection, info) {
-		console.log(`SWARM: New peer: ${info.host} on port: ${info.port}`)
-		try {
-			await afs.download('.')
-		}
-		catch (err) {
-			console.log({downloadErr: err})
-			errorHandler()
-		}
+	} catch(e) {
+		console.log(e)
 	}
 }
 
