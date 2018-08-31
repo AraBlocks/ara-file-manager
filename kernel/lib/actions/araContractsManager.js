3b4adf019f1d51409dcb9b1bac46d271f8684833
ijl0322 <ijl0322@gmail.com>
2018-08-31 11:42:35 -0400

    feature: get User balance in ara

--- kernel/lib/actions/araContractsManager.js
+++ kernel/lib/actions/araContractsManager.js
@@ -1,18 +1,23 @@
 'use strict'
 
+const { abi } = require('ara-contracts/build/contracts/ARAToken.json')
+const { kARATokenAddress } = require('ara-contracts/constants')
 const debug = require('debug')('acm:kernel:lib:actions:araContractsManager')
-const {
-  web3: {
-    account,
-  }
-} = require('ara-util')
 const Web3 = require('web3')
 const PROVIDER = 'ws://127.0.0.1:8546'
 
+const {
+	web3: {
+		account,
+		call,
+  }
+} = require('ara-util')
+
 async function getAccountAddress(owner, password) {
 	try {
-		debug('Getting Account Address ')
+		debug('Getting account address')
 		const acct = await account.load({ did: owner, password })
+		debug(`Account address is ${acct.address}`)
 		return acct.address
 	} catch(e) {
 		debug(e)
@@ -20,6 +25,24 @@ async function getAccountAddress(owner, password) {
 }
 
 async function getUserBalance(account) {
+	debug('Getting account balance')
+	try {
+		const balance = await call({
+			abi,
+			address: '0xd7ba1cafae3349df74a245a15d2a39efeaa25eda', // Remember to change this!!!
+			functionName: 'balanceOf',
+			arguments: [
+				account
+			]
+		})
+		debug(`Balance is ${balance} ARA`)
+		return balance
+	} catch(e) {
+		debug(e)
+	}
+}
+
+async function getEtherBalance(account) {
 	try {
 		const web3 = new Web3(new Web3.providers.WebsocketProvider(PROVIDER))
 		const balanceInWei = await web3.eth.getBalance(account)
