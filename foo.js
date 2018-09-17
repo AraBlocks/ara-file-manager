f4b98d11f3776af2e02a9df34487a0d25b502a3d
Mark Romano <markgromano31@gmail.com>
2018-09-17 16:45:37 -0400

    feature(getEarnings): function to return published item earnings

--- foo.js
+++
@@ -1,4 +0,0 @@
-const araContractsManager = require('./kernel/lib/actions/araContractsManager')
-
-araContractsManager.getEarnings([{meta: { aid: 'e7f92b267eaabfc6c99932f479801cfc0c68f1c361873ba4f132ef56d627a0ba'}}])
-  .then(console.log)
