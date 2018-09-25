542703bc5b64eecadb071e5102a4ae8b39ccbb0b
ijl0322 <ijl0322@gmail.com>
2018-09-25 16:04:33 -0400

    cache

--- kernel/lib/reducers/files.js
+++ kernel/lib/reducers/files.js
@@ -27,6 +27,7 @@ module.exports = (state, { load, type }) => {
       file = state.purchased[state.purchased.length - 1]
       file.downloadPercent = load.downloadPercent
       file.size = load.size || file.size
+      file.status = DOWNLOADING
       break
     case DOWNLOADED:
       file = state.purchased[state.purchased.length - 1]
