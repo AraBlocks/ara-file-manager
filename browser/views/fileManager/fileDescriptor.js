d2171851d3f316795c8dd8e8ef990fa28517947c
Mark Romano <markgromano31@gmail.com>
2018-10-16 13:32:47 -0400

    refactor():filedescriptor
    -restyled filedescriptor
    -circle is no more. will be a line and added in another commit

--- browser/views/fileManager/fileDescriptor.js
+++ browser/views/fileManager/fileDescriptor.js
@@ -24,86 +24,72 @@ class FileDescriptor extends Nanocomponent {
     }
 
     this.children = { hamburger: new Hamburger({}) }
+    this.createSummary = this.createSummary.bind(this)
   }
 
   createSummary({ status, name, size, downloadPercent }) {
-
-    console.log(name)
-    console.log([status, size, downloadPercent])
-    let nameHTML
-    let sizeHTML
-    switch (status) {
-      case k.OUT_OF_SYNC:
-        nameHTML = html`
-          <div class="${styles.nameHolder} fileDescriptor-nameHolder">
-            <div class="${styles.name} fileDescriptor-name">
-              ${[html`<span class="${styles.exclamation} fileDescriptor-exclamation">!</span> `, ' ' + name]}
-            </div>
-          </div>`
-        sizeHTML = html`
-          <div class="${styles.sizeHolder(status)} fileDescriptor-sizeHolder">
-            ${[filesize(size).toLowerCase(), ' ' + '(Out of Sync)']}
+    const nameDiv = [ k.OUT_OF_SYNC, k.UPDATE_AVAILABLE ].includes(status)
+      ? html`
+        <div class="${styles.nameHolder} fileDescriptor-nameHolder">
+          <div class="${styles.name} fileDescriptor-name">
+            ${[html`<span class="${styles.exclamation} fileDescriptor-exclamation">!</span> `, ' ' + name]}
           </div>
-        `
-        break
-      case k.DOWNLOADING:
-        nameHTML = html`
-          <div class="${styles.nameHolder} fileDescriptor-nameHolder">
-            <div class="${styles.name} fileDescriptor-name">
-              ${name}
-            </div>
-          </div>
-        `
-        sizeHTML = html`
-          <div class="${styles.sizeHolder(status)} fileDescriptor-sizeHolder">
-            ${Math.round(filesize(downloadPercent * size).slice(0, -2))} / ${filesize(size).toLowerCase()}
-          </div>
-        `
-        break
-      case k.DOWNLOADED:
-        nameHTML = html`
-          <div class="${styles.nameHolder} fileDescriptor-nameHolder">
-            <div class="${styles.name} fileDescriptor-name">
-              ${name}
-            </div>
-          </div>
-        `
-        sizeHTML = html`
-          <div class="${styles.sizeHolder(status)} fileDescriptor-sizeHolder">
-            ${filesize(size).toLowerCase()}
-          </div>
-        `
-      case k.AWAITING_DOWNLOAD:
-        nameHTML = html`
-          <div class="${styles.nameHolder} fileDescriptor-nameHolder">
-            <div class="${styles.name} fileDescriptor-name">
-              ${name}
-            </div>
-          </div>
-        `
-        sizeHTML = html`
-          <div class="${styles.sizeHolder(status)} fileDescriptor-sizeHolder">
-            ${filesize(size).toLowerCase()}
-          </div>
-        `
-      default://DOWNLOADED_PUBLISHED
-        nameHTML = html`
-          <div class="${styles.nameHolder} fileDescriptor-nameHolder">
-            <div class="${styles.name} fileDescriptor-name">
-              ${name}
-            </div>
-          </div>
-        `
-        sizeHTML = html`
-        <div class="${styles.sizeHolder(status)} fileDescriptor-sizeHolder">
-          ${filesize(size).toLowerCase()}
         </div>
       `
-    }
-    return [nameHTML, sizeHTML]
+      : html`
+        <div class="${styles.nameHolder} fileDescriptor-nameHolder">
+          <div class="${styles.name} fileDescriptor-name">
+            ${name}
+          </div>
+        </div>
+      `
+    const sizeDiv = this.styleSize({ size, status, downloadPercent })
+
+    return [ nameDiv, sizeDiv ]
   }
 
-  styleSize({ size, status })
+  styleSize({ size, status, downloadPercent }) {
+    let spanColor
+    let msg
+    let unitColor
+    let downloadingModifer
+    switch (status) {
+      case k.DOWNLOADED_PUBLISHED:
+        spanColor = 'blue'
+        break
+      case k.AWAITING_DOWNLOAD:
+        spanColor = 'grey'
+        unitColor = 'grey'
+        break
+      case k.OUT_OF_SYNC:
+        spanColor = 'red'
+        msg = '(Out of Sync)'
+        break
+      case k.DOWNLOADING:
+        spanColor = 'red'
+        downloadingModifer = Math.round(filesize(downloadPercent * size).slice(0, -2)) + ' / '
+        break
+      case k.PAUSED:
+        spanColor = 'grey'
+        downloadingModifer = Math.round(filesize(downloadPercent * size).slice(0, -2)) + ' / '
+        msg = '(Paused)'
+      case k.UPDATE_AVAILABLE:
+        spanColor = 'red'
+        msg = '(Update Available)'
+    }
+
+    const [_size, unit] = filesize(size, { output: 'array' })
+    const sizeSpan = html`<span style="color:var(--ara-${spanColor});">${_size}</span>`
+    const unitSpan = html`<span style="color:var(--ara-${unitColor});"> ${unit.toLocaleLowerCase()}</span>`
+    const msgSpan = msg ? html`<span style="color:var(--ara-${spanColor});"> ${msg}</span>` : null
+
+    return html`
+      <div class="${styles.sizeHolder} fileDescriptor-sizeHolder">
+        ${[ downloadingModifer, sizeSpan, unitSpan, msgSpan ]}
+      </div>
+    `
+  }
+
   update() {
     return true
   }
@@ -124,11 +110,11 @@ class FileDescriptor extends Nanocomponent {
         </div>
         <div class="${styles.summaryHolder} fileDescriptor-summaryHolder">
           ${createSummary({
-        status,
-        downloadPercent,
-        name: props.name,
-        size: props.size
-      })}
+            status,
+            downloadPercent,
+            name: props.name,
+            size: props.size
+          })}
         </div>
       </div>
     `
