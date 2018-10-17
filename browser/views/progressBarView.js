88befbee035ce84e53534b4a90f6a295a6bdc792
Mark Romano <markgromano31@gmail.com>
2018-10-17 15:05:36 -0400

    feature: progress bar

--- browser/views/progressBarView.js
+++ browser/views/progressBarView.js
@@ -6,12 +6,38 @@ const ProgressBar = require('../components/progressBar')
 const html = require('choo/html')
 
 class ProgressBarView extends Nanocomponent {
+  constructor() {
+    super()
+    this.state = {
+      downloadPercent: 0,
+      shouldBroadcast: false,
+      status: k.AWAITING_DOWNLOAD
+    }
+    this.timer = null
+    this.animate = this.animate.bind(this)
+  }
+
+  animate() {
+    if (this.timer) return
+    this.timer = setInterval(() => {
+      this.state.downloadPercent += (Math.random() * 1) * .1
+      if (this.state.downloadPercent >= 1) {
+        clearInterval(this.timer)
+        this.state.downloadPercent = 1
+        this.state.shouldBroadcast = true
+        this.state.status = k.DOWNLOADED_PUBLISHED
+      }
+      this.rerender()
+    }, 1000)
+  }
+
   update() {
     return true
   }
 
   createElement() {
-    const bars = [new ProgressBar, new ProgressBar, new ProgressBar]
+    const { animate } = this
+    const animationBar = new ProgressBar
     return html`
       <div style="
         height: 100%;
@@ -21,8 +47,26 @@ class ProgressBarView extends Nanocomponent {
         align-content: center;
         justify-content: center;
       ">
-        ${bars[0].render({ downloadPercent: 1, status: k.DOWNLOADED_PUBLISHED, shouldBroadcast: true })}
+        DOWNLOADED/PUBLISHED & BROADCASTING
+        ${new ProgressBar().render({ downloadPercent: 1, status: k.DOWNLOADED_PUBLISHED, shouldBroadcast: true })}
         <br>
+        DOWNLOADING
+        ${new ProgressBar().render({ downloadPercent: .5, status: k.DOWNLOADING, shouldBroadcast: false })}
+        <br>
+        OUT OF SYNC
+        ${new ProgressBar().render({ downloadPercent: 1, status: k.OUT_OF_SYNC, shouldBroadcast: false })}
+        <br>
+        PAUSED
+        ${new ProgressBar().render({ downloadPercent: .6, status: k.PAUSED, shouldBroadcast: false })}
+        <br>
+        AWAITING DOWNLOAD
+        ${new ProgressBar().render({ downloadPercent: 0, status: k.AWAITING_DOWNLOAD, shouldBroadcast: false })}
+        <br>
+        UPDATE AVAILABLE
+        ${new ProgressBar().render({ downloadPercent: 1, status: k.UPDATE_AVAILABLE, shouldBroadcast: false })}
+        <br>
+        <span onclick=${animate}>▶</span>
+        ${animationBar.render(this.state)}
       </div>
     `
   }
