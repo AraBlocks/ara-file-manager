e24439b64ec58307aaf966192b605614c3af568e
Mark Romano <markgromano31@gmail.com>
2018-10-18 12:46:45 -0400

    removed mockfiles

--- kernel/lib/store/files.js
+++ kernel/lib/store/files.js
@@ -2,8 +2,8 @@ const k = require('../../../lib/constants/stateManagement')
 
 module.exports = {
   loadingLibrary: false,
-  published: [...mockFiles()],
-  purchased: [...mockFiles()]
+  published: [],
+  purchased: []
 }
 
 function mockFiles() {
