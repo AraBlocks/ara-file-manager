82436279aa8633aeb02ebcd6fa501888f29996ee
Isabel Lee <ijl0322@users.noreply.github.com>
2018-11-02 14:17:20 -0400

    feature: update AFS to 0.2.9 (#207)

--- kernel/lib/actions/utils.js
+++ kernel/lib/actions/utils.js
@@ -51,7 +51,13 @@ async function readFileMetadata(did) {
 	}
 }
 
-async function writeFileMetaData({ did, size, title, userDID = '' }) {
+async function writeFileMetaData({
+	did,
+	size,
+	title,
+	userDID = '',
+	password
+}) {
 	try {
 		const fileData = {
 			author: userDID,
@@ -61,7 +67,7 @@ async function writeFileMetaData({ did, size, title, userDID = '' }) {
 		}
 		const fileDataString = JSON.stringify(fileData)
 		debug('Adding file metadata for %s', did)
-		await afs.metadata.writeKey({ did, key: 'fileInfo', value: fileDataString })
+		await afs.metadata.writeKey({ did, key: 'fileInfo', value: fileDataString, password })
 	} catch (e) {
 		debug(e)
 	}
