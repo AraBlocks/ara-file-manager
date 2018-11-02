48e9847bb335b7ce8f7707c385326d8d5a5f7487
Mark Romano <markgromano31@gmail.com>
2018-11-02 14:34:18 -0400

    feature: write files upon boot

--- boot/writeFiles.js
+++ boot/writeFiles.js
@@ -6,22 +6,63 @@ const fs = require('fs')
 const path = require('path')
 const userHome = require('user-home')
 
-module.exports = () => {
-  const araPath = path.resolve(userHome, '.ara')
-  if (fs.existsSync(araPath) === false) {
-    debug('Creating .ara directory')
-    fs.mkdirSync(araPath)
+module.exports = {
+  writeRoot() {
+    const araPath = path.resolve(userHome, '.ara')
+    if (fs.existsSync(araPath) === false) {
+      debug('Creating .ara directory')
+      fs.mkdirSync(araPath)
 
-    debug('Creating keyrings directory')
-    const keyringPath = path.resolve(userHome, '.ara', 'keyrings')
-    fs.mkdirSync(keyringPath)
+      debug('Creating keyrings directory')
+      const keyringPath = path.resolve(userHome, '.ara', 'keyrings')
+      fs.mkdirSync(keyringPath)
 
-    debug('Writing node public key')
-    const pubKeyPath = path.resolve(userHome, '.ara', 'keyrings', k.NODE_PUBLIC_KEY_NAME)
-    const pubKeyData = fs.readFileSync(path.resolve(__dirname, '..', 'lib', 'keyrings', 'keyring.pub'))
-    fs.writeFileSync(pubKeyPath, pubKeyData)
+      debug('Writing node public key')
+      const pubKeyPath = path.resolve(userHome, '.ara', 'keyrings', k.NODE_PUBLIC_KEY_NAME)
+      const pubKeyData = fs.readFileSync(path.resolve(__dirname, '..', 'lib', 'keyrings', k.KEYRING_NAME))
+      fs.writeFileSync(pubKeyPath, pubKeyData)
 
-    return true
+      return true
+    }
+    return false
+  },
+
+  writeAraRC() {
+    const araRCPath = path.resolve(userHome, '.ararc')
+    if (fs.existsSync(araRCPath) === false) {
+      debug('Writing .ararc')
+      const keyringPath = path.resolve(userHome, '.ara', 'keyrings', k.KEYRING_NAME)
+      const araRCData = `
+        ;; Ethereum conf
+        [web3]
+        provider = ${k.PROVIDER_SOCKET_ADDRESS};
+        provider = ${k.PROVIDER_HTTP_ADDRESS};
+        network_id = ${k.NETWORK_ID};
+
+        ;; Identity network configuration
+        [network.identity]
+        keyring=${keyringPath}
+
+        [network.identity.resolver]
+        secret = ${k.RESOLVER_SECRET}
+        keyring = ${keyringPath}
+        network = ${k.RESOLVER_NETWORK}
+
+        [network.identity.archiver]
+        secret = ${k.ARCHIVER_SECRET}
+        keyring = ${keyringPath}
+        network = ${k.RESOLVER_NETWORK}
+
+        [network.dns]
+        server = ${k.NETWORK_DNS}
+
+        [network.dht]
+        bootstrap = ${k.NETWORK_DHT}
+      `
+      fs.writeFileSync(araRCPath, araRCData)
+
+      return true
+    }
+    return false
   }
-  return false
 }
