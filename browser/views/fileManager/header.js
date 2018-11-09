63335f5c9ea8173cba1ad1ea3e9c4d23d6ab2a9e
Mark Romano <markgromano31@gmail.com>
2018-11-09 13:19:39 -0500

    style: tooltip to copy userdid

--- browser/views/fileManager/header.js
+++ browser/views/fileManager/header.js
@@ -2,17 +2,23 @@
 
 
 const Button = require('../../components/button')
+const {	clipboard } = require('electron')
 const { CLEAN_UI } = require('../../../lib/constants/stateManagement')
 const styles = require('./styles/header')
 const UtilityButton = require('../../components/utilityButton')
 const TabItem = require('../../components/tabItem')
-const {
-  emit ,
-  minimizeWindow,
-  openWindow,
-} = require('../../lib/tools/windowManagement')
+const windowManagement = require('../../lib/tools/windowManagement')
 const html = require('choo/html')
 const Nanocomponent = require('nanocomponent')
+const tt = require('electron-tooltip')
+
+let bad = true
+tt({
+  position: 'top',
+  customContent() {
+    return bad ? 'Copy To Clipboard' : 'Copied'
+  }
+})
 
 class Header extends Nanocomponent {
   constructor({ selectTab, userAid }) {
@@ -24,10 +30,10 @@ class Header extends Nanocomponent {
       publishFilebutton: new Button({
         children: 'Publish New File',
         cssClass: { opts: { color: 'blue', fontSize: 14 } },
-        onclick: () => openWindow('publishFileView')
+        onclick: () => windowManagement.openWindow('publishFileView')
       }),
       closeButton: new UtilityButton({ children: '✕' }),
-      minimizeButton: new UtilityButton({ children: '–', onclick: minimizeWindow }),
+      minimizeButton: new UtilityButton({ children: '–', onclick: windowManagement.minimizeWindow }),
       tabs: this.makeTabs(selectTab)
     }
   }
@@ -48,12 +54,11 @@ class Header extends Nanocomponent {
   }
 
   createElement({ activeTab, araBalance }) {
-    console.log('araBalance ',araBalance)
     const { children, props } = this
     return html`
       <div class="${styles.container} header-container">
         <div class="${styles.subHeader} header-subheader">
-          <div onclick=${() => emit({ event: CLEAN_UI })}>
+          <div onclick=${() => windowManagement.emit({ event: CLEAN_UI })}>
             <img style="height: 7px;" src="../assets/images/LTLSTR_Logo_FileManager.png"/>
           </div>
           <div class="${styles.windowControlsHolder} header-windowControlsHolder">
@@ -66,11 +71,15 @@ class Header extends Nanocomponent {
             File Manager
           </div>
           <div class="${styles.userHolder} header-userHolder">
-            <div>
+            <div
+              data-tooltip
+              class="${styles.didHolder} header-didHolder"
+              onclick=${() => clipboard.writeText(props.userDID)}
+            >
               <b>ID: ${props.userDID.slice(8,14)}...</b>
             </div>
             <div>
-              ${araBalance ? Math.round(araBalance * 100) / 100 + ' Ara' : 'Calculating Balance...'} 
+              ${araBalance ? Math.round(araBalance * 100) / 100 + ' Ara' : 'Calculating Balance...'}
             </div>
           </div>
         </div>
