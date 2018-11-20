2b712464270992f627857b9bff2ad4fdae3694db
Mark Romano <markgromano31@gmail.com>
2018-11-20 10:52:06 -0500

    cache

--- browser/views/accountInfo.js
+++ browser/views/accountInfo.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const Button = require('./components/button')
-const TestnetBanner = require('./components/TestnetBanner')
+const Button = require('../components/button')
+const TestnetBanner = require('../components/TestnetBanner')
+const UtilityButton = require('../components/UtilityButton')
 const styles = require('./styles/accountInfo')
 const package = require('../../package.json')
 const html = require('choo/html')
@@ -15,6 +16,11 @@ class AccountInfo extends Nanocomponent {
 
     this.props = { ...props, network, version }
     this.children = {
+      closeButton: new UtilityButton({ children: 'close' }),
+      requestTokensButton: new Button({
+        children: 'Request Tokens',
+        cssClass: { opts: { fontSize: '18px' } },
+      }),
       sendTokensButton: new Button({
         children: 'Send Tokens',
         cssClass: { opts: { fontSize: '18px' } },
@@ -27,9 +33,36 @@ class AccountInfo extends Nanocomponent {
   }
 
   createElement() {
-    const { props } = this
+    const { children, props } = this
     return html`
-      <div class="${} modal"></div>
+      <div class="${styles.container} accountInfo-container">
+        ${network === 'test' ? TestnetBanner() : null}
+        ${children.closeButton.render({ children: 'close'})}
+        <div class="${styles.descriptionSection} accountInfo-descriptionSection">
+          <div>
+            Here you’ll find your Ara ID, Wallet Address, and app info. You can transfer Ara Tokens to other accounts from here as well.
+          </div>
+        </div>
+        <div class="${styles.balanceSection} accountInfo-balanceSection">
+          <div><b>Your wallet:</b></div>
+          <div class="${styles.araBalance} accountInfo-araBalance">
+            ${props.araBalance} <span class="${styles.araBalance} accountInfo-araBalance">ARA</span>
+          </div>
+          <div class="${styles.ethBalance} accountInfo-araBalance">
+            ${props.ethBalance} <span class="${styles.ethBalance} accountInfo-ara">ETH</span>
+          </div>
+        </div>
+        <div class="${styles.interactiveSection} accountInfo-interactiveSection">
+          <div class="${styles.userDID} accountInfo-userDID">
+            <div>Your <b>Ara ID is:</b></div>
+            <div >${props.userDID}</div>
+          </div>
+          <div class="${styles.ethAddress} accountInfo-ethAddress">
+            <div>Your <b>Wallet Address</b> is:</div>
+            <div>${props.ethAddress}</div>
+          </div>
+        </div>
+      </div>
     `
   }
 }
