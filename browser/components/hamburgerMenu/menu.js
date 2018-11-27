146e8c61c4a121fe76307ac253be16d69049c8f0
ijl0322 <ijl0322@gmail.com>
2018-11-27 11:49:28 -0500

    feat(): hide menu on mouseleave

--- browser/components/hamburgerMenu/menu.js
+++ browser/components/hamburgerMenu/menu.js
@@ -10,7 +10,7 @@ class Menu extends Nanocomponent {
 		super()
 		this.props = { items: this.makeButtons(items) }
 		this.state = { displayItems: false }
-		this.menuClicked = this.menuClicked.bind(this)
+		this.toggleMenu = this.toggleMenu.bind(this)
 	}
 
 	makeButtons(items) {
@@ -21,24 +21,28 @@ class Menu extends Nanocomponent {
 		return true
 	}
 
-	menuClicked() {
+	toggleMenu(e) {
 		const { state } = this
-		state.displayItems = !state.displayItems
+		e.type === 'mouseleave'
+			? state.displayItems = false
+			: state.displayItems = !state.displayItems
 		this.render()
 	}
 
 	createElement() {
-		const { props, state, menuClicked } = this
+		const { props, state, toggleMenu, click2, mouseLeave } = this
 
 		return html`
 			<div class="${styles.container} Menu-container"
-				onclick=${menuClicked}
+				onclick=${toggleMenu}
 			>
 				<img
 					class="${styles.hamburger} Menu-hamburger"
 					src="../assets/images/utilityButtons/Hamburger.svg"
 				/>
-				<div class="${styles.menu(state.displayItems)} Menu-menu">
+				<div class="${styles.menu(state.displayItems)} Menu-menu"
+					onmouseleave=${toggleMenu}
+				>
 					${props.items.map(item => [
 						item.render(),
 						html`<div class="${styles.divider} Menu-divider"></div>`
