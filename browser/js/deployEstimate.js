337591de476fef7cb43f56809e6730315f052d91
ijl0322 <ijl0322@gmail.com>
2018-11-28 18:10:34 -0500

    cache

--- browser/js/deployEstimate.js
+++ browser/js/deployEstimate.js
@@ -5,4 +5,4 @@ const { REFRESH } = require('../../lib/constants/stateManagement')
 const deployEstimate = new DeployEstimate({})
 document.getElementById('container').appendChild(deployEstimate.render({}))
 
-ipcRenderer.on(REFRESH, (event, load) => { deployEstimate.render({ estimate: load })} )
+ipcRenderer.on(REFRESH, (event, load) => { deployEstimate.render(load)} )
