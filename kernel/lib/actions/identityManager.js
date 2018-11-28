d17b7b84afc6047321bb460af87d6a4c763030ba
Mark Romano <markgromano31@gmail.com>
2018-11-28 10:33:34 -0500

    minor fixes

--- kernel/lib/actions/identityManager.js
+++ kernel/lib/actions/identityManager.js
@@ -12,9 +12,12 @@ module.exports = {
     try {
       const araId = await aid.create({ context, password })
       await aid.util.writeIdentity(araId)
+
+      context.close()
       return araId
     } catch(e) {
       debug(e)
+      context.close()
     }
   },
 
@@ -33,9 +36,12 @@ module.exports = {
     try {
       const araId = await aid.recover({ context, mnemonic, password})
       await aid.util.writeIdentity(araId)
+
+      context.close()
       return araId
     } catch (err) {
       debug('Err recovering identity: %o', err)
+      context.close()
     }
   },
 
