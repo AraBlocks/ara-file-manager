dc59287bae71ded42ba5a55243c9f3f00712d7c3
Isabel Lee <ijl0322@users.noreply.github.com>
2018-11-29 12:17:36 -0500

    feat(): Reseed Afs after opening (#272)

--- browser/views/afsExplorerView/container.js
+++ browser/views/afsExplorerView/container.js
@@ -3,19 +3,21 @@
 const AfsFileTable = require('../../components/afsFileTable/afsFileTable')
 const Button = require('../../components/button')
 const { emit } = require('../../lib/tools/windowManagement')
-const { EXPORT_FILE } = require('../../../lib/constants/stateManagement')
+const { EXPORT_FILE, CLOSE_AFS_EXPLORER, DOWNLOAD } = require('../../../lib/constants/stateManagement')
 const fileSystemManager = require('../../lib/tools/fileSystemManager')
 const overlay = require('../../components/overlay')
 const UtilityButton = require('../../components/utilityButton')
 const styles = require('./styles/container')
 const html = require('choo/html')
 const Nanocomponent = require('nanocomponent')
+const windowManagement = require('../../lib/tools/windowManagement')
 
 class Container extends Nanocomponent {
 	constructor({
 		afsName,
 		did,
-		fileList
+		fileList,
+		updateAvailable
 	}) {
 		super()
 		this.props = {
@@ -23,7 +25,8 @@ class Container extends Nanocomponent {
 			did
 		}
 		this.state = {
-			fileList
+			fileList,
+			updateAvailable
 		}
 		this.children = {
 			afsFileTable: new AfsFileTable({
@@ -38,10 +41,16 @@ class Container extends Nanocomponent {
 			}),
 			downloadUpdateButton: new Button({
 				children: 'Download Update',
-				cssClass: { name: 'thinBorder' },
-				onclick: this.exportAll.bind(this)
+				cssClass: this.state.updateAvailable ? { name: 'thinBorder'} : { opts: { color: 'orange' } },
+				onclick: this.downloadUpdate.bind(this)
 			}),
-			utilityButton: new UtilityButton({})
+			utilityButton: new UtilityButton({ onclick: () => {
+				emit({
+					event: CLOSE_AFS_EXPLORER,
+					load: { did }
+				})
+				windowManagement.closeWindow('afsExplorerView')
+			}})
 		}
 	}
 
@@ -64,7 +73,14 @@ class Container extends Nanocomponent {
 	}
 
 	downloadUpdate() {
-
+		const { props } = this
+		emit({
+			event: DOWNLOAD,
+			load: {
+				did: props.did,
+			}
+		})
+		windowManagement.closeWindow('afsExplorerView')
 	}
 
 	update({ fileList }){
