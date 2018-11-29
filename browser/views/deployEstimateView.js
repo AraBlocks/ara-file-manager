994ed253275522b4b4f8e994faa48020cd9cedc0
ijl0322 <ijl0322@gmail.com>
2018-11-29 11:04:56 -0500

    feat(): Add tray support

--- browser/views/deployEstimateView.js
+++ browser/views/deployEstimateView.js
@@ -1,27 +1,28 @@
 'use strict'
 
+const Button = require('../components/button')
+const { closeModal, closeWindow, emit } = require('../lib/tools/windowManagement')
+const k = require('../../lib/constants/stateManagement')
+const { utils } = require('../lib/tools')
 const styles = require('./styles/deployEstimateView')
 const html = require('choo/html')
 const Nanocomponent = require('nanocomponent')
-
-const k = require('../../lib/constants/stateManagement')
-const { closeModal, closeWindow, emit } = require('../lib/tools/windowManagement')
-const Button = require('../components/button')
-const { utils } = require('../lib/tools')
 
 class DeployEstimate extends Nanocomponent {
 	constructor() {
 		super()
 		this.props = {
-			gasEstimate: null,
 			did: null
 		}
 		this.children = {
 			publishButton: new Button({
 				children: 'Confirm',
+				cssClass: { name: 'thinBorder' },
 				onclick: () => {
-					emit({ event: k.CONFIRM_DEPLOY_PROXY, load: { did: this.props.did } }),
-					closeModal()
+					if (this.props.did) {
+						emit({ event: k.CONFIRM_DEPLOY_PROXY, load: { did: this.props.did } }),
+						closeModal()
+					}
 				}
 			}),
 			cancelbutton: new Button({
@@ -37,7 +38,6 @@ class DeployEstimate extends Nanocomponent {
 	update({ did }){
 		const { props } = this
 		props.did = did
-		console.log(did)
 		return true
 	}
 
@@ -45,11 +45,11 @@ class DeployEstimate extends Nanocomponent {
 		let estimateText = html``
 		estimate
 			? estimateText = html `
-				<span class="${styles.postheader} modals-postheader">
+				<span class="${styles.postheader} deployEstimateView-postheader">
 					${utils.roundDecimal(estimate, 1000)} eth
 				</span>`
 			: estimateText = html `
-				<div class="${styles.spinnerHolder} modal-spinnerHolder">
+				<div class="${styles.spinnerHolder} deployEstimateView-spinnerHolder">
 					<div class="spinner-small-red ${styles.spinnerHolder}"></div>
 				</div>
 			`
@@ -57,18 +57,18 @@ class DeployEstimate extends Nanocomponent {
 	}
 
 	createElement({ estimate = null }) {
-		const { props, children, renderEstimate } = this 
+		const { children, renderEstimate } = this
 		return html`
-		<div class="${styles.container} modals-container">
+		<div class="${styles.container} deployEstimateView-container">
       <div>
-        <div class="${styles.messageBold} ${styles.bottomMargin} modal-messageBold/bottomMargin">
+        <div class="${styles.messageBold} ${styles.bottomMargin} deployEstimateView-messageBold/bottomMargin">
           Start Publishing?
         </div>
-        <div class="${styles.verticalContainer} modal-verticalContainer">
-          <div class="${styles.smallMessage({})} modal-smallMessage">
-						In order to start the publishing process,<br> 
-						the File Manager needs to prepare the network.<br>
-            <br>This will cost:
+        <div class="${styles.verticalContainer} deployEstimateView-verticalContainer">
+          <div class="${styles.smallMessage({})} deployEstimateView-smallMessage">
+						In order to start the publishing process,<br>
+						the File Manager needs to prepare the network.<br><br>
+            This will cost:
 					</div>
 					<div class=${styles.estimateHolder}>
 						${renderEstimate(estimate)}
@@ -76,7 +76,12 @@ class DeployEstimate extends Nanocomponent {
 				</div>
 			</div>
 			<div class="${styles.buttonHolder} deployEstimateView-buttonHolder">
-      	${children.publishButton.render({})}
+      	${children.publishButton.render({ cssClass:
+					estimate
+						? { opts: { color: 'teal' } }
+						: { name: 'thinBorder'}
+					})
+				}
       	${children.cancelbutton.render({})}
 			</div>
     </div>
