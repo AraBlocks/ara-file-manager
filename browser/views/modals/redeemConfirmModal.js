2a564ff24fefa8824f94491a3ff45fc8cafae7ae
Isabel Lee <ijl0322@users.noreply.github.com>
2018-12-04 10:36:29 -0500

    feat(): PE-682 Redeem Rewards Confirm Modal (#288)

---
+++ browser/views/modals/redeemConfirmModal.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Button = require('../../components/button')
+const { CONFIRM_REDEEM } = require('../../../lib/constants/stateManagement')
+const windowManagement = require('../../lib/tools/windowManagement')
+const styles = require('./styles')
+const html = require('choo/html')
+
+module.exports = ({ estimate, did }) => {
+  const downloadButton = new Button({
+    children: 'Confirm',
+    onclick: () => {
+      windowManagement.emit({ event: CONFIRM_REDEEM, load: { did } })
+      windowManagement.closeModal('redeemConfirmModal')
+    }
+  })
+  const cancelbutton = new Button({
+    ...styles.buttonSelector('cancel'),
+    onclick: () => windowManagement.closeModal('redeemConfirmModal')
+  })
+  return html`
+    <div class="${styles.container({})} modals-container">
+      <div class="${styles.messageBold} modal-messageBold">
+        Redeem your rewards?
+      </div>
+      <div class="${styles.verticalContainer} modal-verticalContainer">
+        <div class="${styles.smallMessage({})} modal-smallMessage">
+          This will cost:
+        </div>
+        <span class="${styles.postheader} modals-postheader">${estimate} eth</span>
+      </div>
+      <div>
+        ${downloadButton.render()}
+        ${cancelbutton.render()}
+      </div>
+    </div>
+  `
+}
