4bc87af0981962257ada7b084b1fd13261b50038
Isabel Lee <ijl0322@users.noreply.github.com>
2018-12-13 15:11:04 -0500

    feat(): PE-726 Peer count live update(#323)

--- kernel/redux/actionCreators/seed.js
+++ kernel/redux/actionCreators/seed.js
@@ -5,10 +5,15 @@ const { farmerManager } = require('../actions')
 const dispatch = require('../reducers/dispatch')
 const k = require('../../../lib/constants/stateManagement')
 const { ipcMain } = require('electron')
+const { internalEmitter } = require('electron-window-manager')
 const windowManager = require('electron-window-manager')
 const store = windowManager.sharedData.fetch('store')
 
-ipcMain.on(k.START_SEEDING, (event, load) => {
+ipcMain.on(k.START_SEEDING, (event, load) => _startSeeding(load))
+
+internalEmitter.on(k.START_SEEDING, load => _startSeeding(load))
+
+function _startSeeding(load) {
   debug('%s HEARD', k.START_SEEDING)
   debug
   const { farmer } = store
@@ -19,7 +24,7 @@ ipcMain.on(k.START_SEEDING, (event, load) => {
   } catch (err) {
     debug('Error starting seed: %o', err)
   }
-})
+}
 
 ipcMain.on(k.STOP_SEEDING, (event, load) => {
   debug('%s HEARD', k.STOP_SEEDING)
