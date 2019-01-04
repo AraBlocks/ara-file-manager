a8e26d77a96ab967db8cd73e9c0971e83f0b2d27
mark g romano <markgromano31@gmail.com>
2019-01-04 16:22:04 -0500

    feat(dispatch): type check added

--- kernel/redux/reducers/dispatch.js
+++ kernel/redux/reducers/dispatch.js
@@ -11,20 +11,21 @@ const state = require('../store')
 const windowManager = require('electron-window-manager')
 
 const reducers = [
-  { property: 'application', reducer: application},
+  { property: 'application', reducer: application },
   { property: 'account', reducer: account },
   { property: 'files', reducer: files },
   { property: 'modal', reducer: modal },
   { property: 'subscriptions', reducer: subscriptions },
-  { property: 'farmer', reducer: farmer}
+  { property: 'farmer', reducer: farmer }
 ]
 
 module.exports = (action) => {
   try {
-    if (!action.type){
+    if (action.hasOwnProperty('type') === false) {
       throw new TypeError('Dispatch object missing the necessary "type" property')
-    }
-    if (action.load && typeof action.load !== 'object') {
+    } else if (action.type === undefined) {
+      throw new TypeError('Value of type property is undefined. You likely forgot to define the constant')
+    } else if (action.load && typeof action.load !== 'object') {
       throw new TypeError('Dispatch object load property should be an object')
     }
   } catch (err) {
