26303f2ee903762a637ffef6059d063653f6d1b0
Mark Romano <markgromano31@gmail.com>
2019-01-08 15:06:39 -0500

    minor fixes

--- kernel/redux/actionCreators/publish.js
+++ kernel/redux/actionCreators/publish.js
@@ -108,7 +108,8 @@ ipcMain.on(k.PUBLISH, async (event, load) => {
     dispatch({ type: k.FEED_MODAL, load: dispatchLoad })
     windowManager.openModal('generalPleaseWaitModal')
     windowManager.closeWindow('manageFileView')
-    // await afsManager.removeAllFiles({ did, password })
+
+    await afsManager.removeAllFiles({ did, password })
     await (await afs.add({ did, paths: load.paths, password })).close()
 
     const size = load.paths.reduce((sum, file) => sum += fs.statSync(file).size, 0)
