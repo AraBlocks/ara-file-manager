61ffbd02573bd0b22de7620f5b22c6decb18e9c2
Mark Romano <markgromano31@gmail.com>
2019-01-09 11:47:48 -0500

    cache

--- browser/js/estimateSpinner/initProps.js
+++ browser/js/estimateSpinner/initProps.js
@@ -10,6 +10,6 @@ module.exports = {
     confirmEvent: k.CONFIRM_DEPLOY_PROXY,
     confirmText: 'Confirm',
     header: 'Start Publishing?',
-    smallMessageText: 'In order to start the publishing process the File Manager needs to prepare a contract on the network.This will cost:'
+    smallMessageText: 'In order to start the publishing process the File Manager needs to prepare a contract on the network'
   }
 }
