80d2ec71ca0d8b8a2bc9da435fc97cb791590d25
Mark Romano <markgromano31@gmail.com>
2019-01-10 12:52:27 -0500

    smol-fix

--- browser/views/estimateSpinner.js
+++ browser/views/estimateSpinner.js
@@ -75,6 +75,7 @@ class EstimateSpinner extends Nanocomponent {
       props,
       confirmButtonStyle
     } = this
+    console.log(this.props)
     return html`
       <div class="${styles.container} EstimateSpinner-container">
         <div class="${styles.topContainer} estimateSpinner-topContainer">
