702e4165f4fa726f0f778b217471bd220635a9b1
Mark Romano <markgromano31@gmail.com>
2019-01-11 14:06:40 -0500

    feat(): backup eth faucy

---
+++ kernel/redux/actionCreators/register/register.helpers.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const debug = require('debug')('afm:kernel:lib:actionCreators:register')
+const { utils } = require('../../actions')
+
+async function requestEther(ethAddress) {
+  try {
+    debug('Requesting from main eth faucet')
+    await utils.requestEthFaucet(ethAddress)
+  } catch (err) {
+    debug('Error requesting from main eth faucet: %s', err.message)
+    try {
+      debug('Requesting from fallback eth faucet')
+      await utils.requestFallbackEthFaucet(ethAddress)
+    } catch (err) {
+      debug('Error requesting from fallback eth faucet: %s', err.message)
+    }
+  }
+}
+
+module.exports = {
+  requestEther
+}
