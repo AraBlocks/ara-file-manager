8518241c5bc7def61cc42f0e6bde7812d8a4f7bd
Mark Romano <markgromano31@gmail.com>
2019-01-29 16:18:54 -0500

    added installer asset

--- scripts/winstaller.js
+++ scripts/winstaller.js
@@ -5,8 +5,8 @@ winstaller.createWindowsInstaller({
   outputDirectory: '/release-builds/winstalled',
   authors: 'Ara blocks',
   exe: 'Ara File Manager.exe',
-  iconUrl: `https://s3.amazonaws.com/ara-prod-media/file-manager/ara_prod.ico`,
-  loadingGif: '../build/installer.gif',
+  iconUrl: 'https://s3.amazonaws.com/ara-prod-media/file-manager/ara_prod.ico',
+  loadingGif: '../build/Ara-Installing.gif',
   setupExe: 'AFM Installer.exe'
 })
   .then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`))
