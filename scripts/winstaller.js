48040f506a8517050cd200af3188c3d4f0f3c72a
Charles Kelly <cckelly21@gmail.com>
2019-01-30 11:26:37 -0500

    refactor(winstaller): changed icons

--- scripts/winstaller.js
+++ scripts/winstaller.js
@@ -1,12 +1,13 @@
 const winstaller = require('electron-winstaller')
 
 winstaller.createWindowsInstaller({
-  appDirectory: '/release-builds/Ara File Manager-win32-x64/Ara File Manager.exe',
-  outputDirectory: '/release-builds/winstalled',
+  appDirectory: './release-builds/Ara-File-Manager-win32-x64/',
+  outputDirectory: './release-builds/winstalled',
   authors: 'Ara blocks',
   exe: 'Ara-File-Manager.exe',
   iconUrl: 'https://s3.amazonaws.com/ara-prod-media/file-manager/ara_prod.ico',
-  loadingGif: '../build/Ara-Installing.gif',
+  setupIcon: './build/icons/windows/ara_prod.ico',
+  loadingGif: './build/Ara-Installing.gif',
   setupExe: 'AFM Installer.exe'
 })
   .then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`))
