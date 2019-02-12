c30688469e8062bff8d7bdfd56920a3ccf064924
vipyne <vipyne@gmail.com>
2019-02-12 14:13:41 -0500

    chore(): top of file linting

--- kernel/redux/actionCreators/download.js
+++ kernel/redux/actionCreators/download.js
@@ -6,6 +6,10 @@ const { ipcMain } = require('electron')
 const { stateManagement: k } = require('k')
 const windowManager = require('electron-window-manager')
 const store = windowManager.sharedData.fetch('store')
+const windowManager = require('electron-window-manager')
+
+const { farmerManager } = require('../actions')
+const utils = require('../actions/utils')
 
 const { farmerManager } = require('../actions')
 const utils = require('../actions/utils')
