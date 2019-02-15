04f5728dc67bee7502f05e9f9fad473a6ae262ae
Mark Romano <markgromano31@gmail.com>
2019-02-15 15:47:20 -0500

    feat(progressBar): added message to :ant:

--- browser/views/fileManager/styles/progressBar.js
+++ browser/views/fileManager/styles/progressBar.js
@@ -1,21 +1,43 @@
 const { colorSelector } = require('styleUtils')
 const { css } = require('css')
+const { stateManagement: k } = require('k')
 
 module.exports = {
   colorSelector,
 
-  holder: css`
-    :host {
-      background-color: ${colorSelector('grey')};
-      height: 1px;
-      // margin-bottom: 20px;
-      width: 100%;
+  holder(status = null) {
+    return css`
+      :host {
+        background-color: ${colorSelector('grey')};
+        height: 1px;
+        width: 100%;
+        position: relative;
+      }
 
-    }
+      :host > div {
+        height: 1px;
+        transition: width 100ms;
+      }
 
-    :host > div {
-      height: 1px;
-      transition: width 100ms;
-    }
-  `,
+      ${status &&
+      `:host::before {
+          content: "${(
+            status === k.PURCHASING
+              ? 'Purchasing'
+              : status === k.PUBLISHING
+                ? 'Publishing'
+                : 'Connecting'
+          )}";
+          position: absolute;
+          width: 100%;
+          top: -18px;
+          left: 0;
+          display: flex;
+          justify-content: center;
+          font-size: 14px;
+          color: ${colorSelector('grey')};
+        }`
+      }
+    `
+  },
 }
