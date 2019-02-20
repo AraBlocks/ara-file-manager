2f0405fe0ee333b96945db5d4335c7cd51011a20
Madeline Cameron <github@maddie.today>
2019-02-20 12:47:26 -0500

    refactor(): Fix remaining constant imports

--- kernel/daemons/index.js
+++ kernel/daemons/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   afmManager: require('./afm'),
   acmManager: require('./acm'),
-  utils: require('./utils'),
   afsManager: require('./fs'),
-  identityManager: require('./aid'),
   farmerManager: require('./dcdn'),
+  identityManager: require('./aid'),
+  utils: require('./utils'),
 }
