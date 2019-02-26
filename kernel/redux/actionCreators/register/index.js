05171e9e9d4c8b8c88d6ce96eeb0026d1f08b871
Mark Romano <markgromano31@gmail.com>
2019-02-26 11:32:35 -0500

    fix(tray): created proper flow for register via tray

--- kernel/redux/actionCreators/register/index.js
+++ kernel/redux/actionCreators/register/index.js
@@ -8,35 +8,9 @@ const { identityManager } = require('../../actions')
 const dispatch = require('../../reducers/dispatch')
 const helpers = require('./register.helpers')
 
-ipcMain.on(k.CREATE_USER_DID, async () => {
-  debug('%s heard', k.CREATE_USER_DID)
-  try {
-    const identityProps = await helpers.createIdentity()
-    dispatch({
-      type: k.CREATED_USER_DID,
-      load: {
-        ...identityProps,
-        araBalance: 0,
-        ethBalance: 0,
-      }
-    })
-    windowManager.pingView({
-      view: 'registration',
-      event: k.CREATED_USER_DID,
-      load: {
-        userDID: identityProps.userDID,
-        mnemonic: identityProps.mnemonic
-      }
-    })
-    const subscriptions = await helpers.getSubscriptions(identityProps)
-    dispatch({ type: k.GOT_REGISTRATION_SUBS, load: subscriptions })
-  } catch (err) {
-    debug('Error creating identity: %o', err)
-    dispatch({ type: k.FEED_MODAL, load: { modalName: 'registrationFailed' } })
-    windowManager.openModal('generalMessageModal')
-    windowManager.closeWindow('registration')
-  }
-})
+ipcMain.on(k.CREATE_USER_DID, helpers.pushAID)
+
+windowManager.internalEmitter.on(k.CREATE_USER_DID, helpers.pushAID)
 
 ipcMain.on(k.REGISTER, async (_, { mnemonic, password, userDID }) => {
   debug('%s heard', k.REGISTER)
