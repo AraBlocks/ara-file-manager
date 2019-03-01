0fe640419bd6a092a3ef0b851425c60d1c7bddf3
vipyne <vipyne@gmail.com>
2019-02-28 19:11:16 -0500

    feat(browser/js/): Send PAGE_VIEW events

--- browser/js/mnemonicWarning.js
+++ browser/js/mnemonicWarning.js
@@ -1,8 +1,16 @@
 const MnemonicWarning = require('../views/mnemonicWarning')
-const { remote } = require('electron')
+const { ipcRenderer, remote } = require('electron')
+const { PAGE_VIEW } = require('../../lib/constants/stateManagement')
+
 const windowManager = remote.require('electron-window-manager')
 const store = windowManager.sharedData.fetch('store')
 
 const { modal } = store
 const mnemonicWarning = new MnemonicWarning(modal.data)
 document.getElementById('container').appendChild(mnemonicWarning.render())
+
+if (modal.data.isAFS) {
+  ipcRenderer.send(PAGE_VIEW, { view: 'mnemonicWarning/AFS' })
+} else {
+  ipcRenderer.send(PAGE_VIEW, { view: 'mnemonicWarning' })
+}
