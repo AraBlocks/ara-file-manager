64d6732952a06842ae9d815430f34a20449151c8
vipyne <vipyne@gmail.com>
2019-02-28 19:05:56 -0500

    feat(windowManagement|modal): Trigger PAGE_VIEW and modal listens for

--- kernel/redux/actionCreators/modal.js
+++ kernel/redux/actionCreators/modal.js
@@ -1,5 +1,10 @@
 const dispatch = require('../reducers/dispatch')
-const { DUMP_MODAL_STATE } = require('../../../lib/constants/stateManagement')
+const { DUMP_MODAL_STATE, PAGE_VIEW } = require('../../../lib/constants/stateManagement')
 const { ipcMain } = require('electron')
+const analytics = require('../actions/analytics')
 
 ipcMain.on(DUMP_MODAL_STATE, () => dispatch({ type: DUMP_MODAL_STATE }))
+
+ipcMain.on(PAGE_VIEW, (event, load) => {
+  analytics.trackScreenView(load.view)
+})
