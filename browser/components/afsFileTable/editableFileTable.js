5af6c0b9d3bee05c64c0c2f83903ffaf69560656
Madeline Cameron <github@maddie.today>
2019-03-06 13:10:58 -0500

    fix(): Fix file add for Linux

--- browser/components/afsFileTable/editableFileTable.js
+++ browser/components/afsFileTable/editableFileTable.js
@@ -7,207 +7,208 @@ const html = require('nanohtml')
 const Nanocomponent = require('nanocomponent')
 
 const IS_WINDOWS = process.platform === 'win32'
+const IS_LINUX = process.platform === 'linux'
 
 class EditableFileTable extends Nanocomponent {
-	constructor(opts) {
-		super()
-		this.props = opts
-		this.state = {
-			sortNameReversed: true,
-			sortSizeReversed: false,
-			sortTypeReversed: false
-		}
-		this.children = {
-			sortFileButton: new UtilityButton({
-				children: 'upArrow',
-				onclick: this.sortFileName.bind(this)
-			}),
-			sortTypeButton: new UtilityButton({
-				children: 'downArrow',
-				onclick: this.sortFileType.bind(this)
-			}),
-			sortSizeButton: new UtilityButton({
-				children: 'downArrow',
-				onclick: this.sortFileSize.bind(this)
-			}),
-		}
-		this.deleteFile = this.deleteFile.bind(this)
-		this.getFiles = this.getFiles.bind(this)
-		this.getFolders = this.getFolders.bind(this)
-		this.onFileDrop = this.onFileDrop.bind(this)
-		this.preventDefault = this.preventDefault.bind(this)
-		this.renderAddOpts = this.renderAddOpts.bind(this)
-	}
+  constructor(opts) {
+    super()
+    this.props = opts
+    this.state = {
+      sortNameReversed: true,
+      sortSizeReversed: false,
+      sortTypeReversed: false
+    }
+    this.children = {
+      sortFileButton: new UtilityButton({
+        children: 'upArrow',
+        onclick: this.sortFileName.bind(this)
+      }),
+      sortTypeButton: new UtilityButton({
+        children: 'downArrow',
+        onclick: this.sortFileType.bind(this)
+      }),
+      sortSizeButton: new UtilityButton({
+        children: 'downArrow',
+        onclick: this.sortFileSize.bind(this)
+      }),
+    }
+    this.deleteFile = this.deleteFile.bind(this)
+    this.getFiles = this.getFiles.bind(this)
+    this.getFolders = this.getFolders.bind(this)
+    this.onFileDrop = this.onFileDrop.bind(this)
+    this.preventDefault = this.preventDefault.bind(this)
+    this.renderAddOpts = this.renderAddOpts.bind(this)
+  }
 
-	update() {
-		return true
-	}
+  update() {
+    return true
+  }
 
-	makeFileRows() {
-		const { props, deleteFile } = this
-		return props.parentState[props.field].map(fileInfo => new AfsFileRow({
-			did: props.did,
-			fileInfo,
-			deleteFile,
-			rowType: props.tableType
-		}))
-	}
+  makeFileRows() {
+    const { props, deleteFile } = this
+    return props.parentState[props.field].map(fileInfo => new AfsFileRow({
+      did: props.did,
+      fileInfo,
+      deleteFile,
+      rowType: props.tableType
+    }))
+  }
 
-	preventDefault(e) {
-		e.preventDefault()
-		e.stopPropagation()
-	}
+  preventDefault(e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
 
-	onFileDrop(e) {
-		this.preventDefault(e)
-		const { props } = this
-		const rawFileData = e.dataTransfer.files
-		const fileData = Array.from(rawFileData).map(file =>
-			({
-				isFile: file.type !== "",
-				subPath: file.name,
-				fullPath: file.path,
-				size: file.size
-			})
-		)
-		props.parentState[props.field].push(...fileData)
-		props.renderView()
-	}
+  onFileDrop(e) {
+    this.preventDefault(e)
+    const { props } = this
+    const rawFileData = e.dataTransfer.files
+    const fileData = Array.from(rawFileData).map(file =>
+      ({
+        isFile: file.type !== "",
+        subPath: file.name,
+        fullPath: file.path,
+        size: file.size
+      })
+    )
+    props.parentState[props.field].push(...fileData)
+    props.renderView()
+  }
 
-	deleteFile(fileInfo) {
-		const { props } = this
-		props.parentState[props.field] = props.parentState[props.field].filter(file =>
-			fileInfo.fullPath == null
-				? file.subPath !== fileInfo.subPath
-				: file.fullPath !== fileInfo.fullPath
-		)
-		props.renderView()
-	}
+  deleteFile(fileInfo) {
+    const { props } = this
+    props.parentState[props.field] = props.parentState[props.field].filter(file =>
+      fileInfo.fullPath == null
+      ? file.subPath !== fileInfo.subPath
+      : file.fullPath !== fileInfo.fullPath
+    )
+    props.renderView()
+  }
 
-	async getFiles() {
-		let files
-		IS_WINDOWS
-			? files = await fileSystemManager.showSelectFileDialog()
-			: files = await fileSystemManager.showSelectFileAndFolderDialog()
-		this.props.addItems(files)
-	}
+  async getFiles() {
+    let files
+    (IS_WINDOWS || IS_LINUX)
+      ? files = await fileSystemManager.showSelectFileDialog()
+      : files = await fileSystemManager.showSelectFileAndFolderDialog()
+    this.props.addItems(files)
+  }
 
-	async getFolders() {
-		const folders = await fileSystemManager.showSelectDirectoryDialog()
-		this.props.addItems(folders)
-	}
+  async getFolders() {
+    const folders = await fileSystemManager.showSelectDirectoryDialog()
+    this.props.addItems(folders)
+  }
 
-	renderAddOpts(length) {
-		const { getFiles, getFolders } = this
-		return length
-			? html`
-				<div class="${styles.addOptions} editableFileTable-addOptions">
-					<span class="${styles.add()} editableFileTable-add" onclick="${getFiles}">
-						Add file +
-					</span>
-					<span class="${styles.add(IS_WINDOWS)} editableFileTable-add" onclick="${getFolders}">
-						Add folder +
-					</span>
-				</div>
-			`
-			: html`
-				<div class="${styles.dragDropMsg} editableFileTable-dragDropMsg">
-					Drop files here
-					<div>
-						Or 
-						<span class="${styles.add()} editableFileTable-add" onclick="${getFiles}">
-							add files
-						</span>
-						<span class="${styles.add(IS_WINDOWS)} editableFileTable-add" onclick="${getFolders}">
-							/ add folders
-						</span>
-						from finder
-					</div>
-				</div>
-			`
-	}
+  renderAddOpts(length) {
+    const { getFiles, getFolders } = this
+    return length
+      ? html`
+        <div class="${styles.addOptions} editableFileTable-addOptions">
+          <span class="${styles.add()} editableFileTable-add" onclick="${getFiles}">
+            Add file +
+          </span>
+          <span class="${styles.add(IS_WINDOWS || IS_LINUX)} editableFileTable-add" onclick="${getFolders}">
+            Add folder +
+          </span>
+        </div>
+      `
+      : html`
+        <div class="${styles.dragDropMsg} editableFileTable-dragDropMsg">
+          Drop files here
+          <div>
+            Or
+            <span class="${styles.add()} editableFileTable-add" onclick="${getFiles}">
+              add files
+            </span>
+            <span class="${styles.add(IS_WINDOWS || IS_LINUX)} editableFileTable-add" onclick="${getFolders}">
+              / add folders
+            </span>
+            from finder
+          </div>
+        </div>
+      `
+  }
 
-	sortFileName() {
-		const { props, state } = this
-		fileListSorter.sortTextAttribute({
-			fileList: props.parentState[props.field],
-			attribute: 'subPath',
-			reversed: state.sortNameReversed
-		})
-		state.sortNameReversed = !state.sortNameReversed
-		props.renderView()
-	}
+  sortFileName() {
+    const { props, state } = this
+    fileListSorter.sortTextAttribute({
+      fileList: props.parentState[props.field],
+      attribute: 'subPath',
+      reversed: state.sortNameReversed
+    })
+    state.sortNameReversed = !state.sortNameReversed
+    props.renderView()
+  }
 
-	sortFileSize() {
-		const { props, state } = this
-		fileListSorter.sortNumericAttribute({
-			fileList: props.parentState[props.field],
-			attribute: 'size',
-			reversed: state.sortSizeReversed
-		})
-		state.sortSizeReversed = !state.sortSizeReversed
-		props.renderView()
-	}
+  sortFileSize() {
+    const { props, state } = this
+    fileListSorter.sortNumericAttribute({
+      fileList: props.parentState[props.field],
+      attribute: 'size',
+      reversed: state.sortSizeReversed
+    })
+    state.sortSizeReversed = !state.sortSizeReversed
+    props.renderView()
+  }
 
-	sortFileType() {
-		const { props, state } = this
-		fileListSorter.sortFileType({
-			fileList: props.parentState[props.field],
-			reversed: state.sortTypeReversed
-		})
-		state.sortTypeReversed = !state.sortTypeReversed
-		props.renderView()
-	}
+  sortFileType() {
+    const { props, state } = this
+    fileListSorter.sortFileType({
+      fileList: props.parentState[props.field],
+      reversed: state.sortTypeReversed
+    })
+    state.sortTypeReversed = !state.sortTypeReversed
+    props.renderView()
+  }
 
-	createElement() {
-		const {
-			children,
-			preventDefault,
-			onFileDrop,
-			state,
-			renderAddOpts
-		} = this
+  createElement() {
+    const {
+      children,
+      preventDefault,
+      onFileDrop,
+      state,
+      renderAddOpts
+    } = this
 
-		const fileRows = this.makeFileRows()
-		return html`
-			<div
-				class="${styles.container} editableFileTable-container"
-				ondrop="${onFileDrop}"
-				ondragover="${preventDefault}"
-			 	ondragenter="${preventDefault}"
-				ondragleave="${preventDefault}"
-			>
-				<table class="${styles.fileTable} EditableFileTable-container">
-					<thead>
-						<tr>
-							<th style="width: 350px;">
-								<div class="${styles.headerHolder} EditableFileTable-headerHolder">
-									Name
-									${children.sortFileButton.render({ children: state.sortNameReversed ? 'upArrow' : 'downArrow' })}
-								</div>
-							</th>
-							<th style="width: 99px;">
-								<div class="${styles.headerHolder} EditableFileTable-headerHolder">
-									Type
-									${children.sortTypeButton.render({ children: state.sortTypeReversed ? 'upArrow' : 'downArrow' })}
-								</div>
-							</th>
-							<th style="width: 99px;">
-								<div class="${styles.headerHolder} EditableFileTable-headerHolder">
-									Size
-									${children.sortSizeButton.render({ children: state.sortSizeReversed ? 'upArrow' : 'downArrow' })}
-								</div>
-							</th>
-						</tr>
-					</thead>
-					<tbody style="height: ${fileRows.length ? 285 : 0}px;">
-						${fileRows.map((fileRow, index) => fileRow.render(index))}
-					</tbody>
-				</table>
-				${renderAddOpts(fileRows.length)}
-			</div>
-		`
-	}
+    const fileRows = this.makeFileRows()
+    return html`
+      <div
+        class="${styles.container} editableFileTable-container"
+        ondrop="${onFileDrop}"
+        ondragover="${preventDefault}"
+        ondragenter="${preventDefault}"
+        ondragleave="${preventDefault}"
+      >
+        <table class="${styles.fileTable} EditableFileTable-container">
+          <thead>
+            <tr>
+              <th style="width: 350px;">
+                <div class="${styles.headerHolder} EditableFileTable-headerHolder">
+                  Name
+                  ${children.sortFileButton.render({ children: state.sortNameReversed ? 'upArrow' : 'downArrow' })}
+                </div>
+              </th>
+              <th style="width: 99px;">
+                <div class="${styles.headerHolder} EditableFileTable-headerHolder">
+                  Type
+                  ${children.sortTypeButton.render({ children: state.sortTypeReversed ? 'upArrow' : 'downArrow' })}
+                </div>
+              </th>
+              <th style="width: 99px;">
+                <div class="${styles.headerHolder} EditableFileTable-headerHolder">
+                  Size
+                  ${children.sortSizeButton.render({ children: state.sortSizeReversed ? 'upArrow' : 'downArrow' })}
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody style="height: ${fileRows.length ? 285 : 0}px;">
+            ${fileRows.map((fileRow, index) => fileRow.render(index))}
+          </tbody>
+        </table>
+        ${renderAddOpts(fileRows.length)}
+      </div>
+    `
+  }
 }
 
 module.exports = EditableFileTable
--- browser/views/manageFile/fileInfo.js
+++ browser/views/manageFile/fileInfo.js
@@ -7,88 +7,88 @@ const html = require('nanohtml')
 const Nanocomponent = require('nanocomponent')
 
 class FileInfo extends Nanocomponent {
-	constructor(props) {
-		const {
-			addItems,
-			did,
-			parentState,
-			renderView
-		} = props
+  constructor(props) {
+    const {
+      addItems,
+      did,
+      parentState,
+      renderView
+    } = props
 
-		super()
+    super()
 
-		this.props = { did, parentState, renderView }
-		this.children = {
-			fileTable: new FileTable({
-				addItems,
-				did,
-				parentState,
-				field: 'fileList',
-				tableType: events.UPDATE_FILE,
-				renderView
-			}),
-			fileNameInput: new ErrorInput({
-				field: 'name',
-				placeholder: 'File Name',
-				parentState,
-				renderView
-			}),
-			fileSelector: new FileSelector({
-				field: 'filePath',
-				parentState
-			}),
-			priceInput: new ErrorInput({
-				araIcon: true,
-				errorMessage: 'Price cannot be negative',
-				field: 'price',
-				parentState,
-				placeholder: 'Price',
-				type: 'number',
-				renderView
-			})
-		}
-	}
+    this.props = { did, parentState, renderView }
+    this.children = {
+      fileTable: new FileTable({
+        addItems,
+        did,
+        parentState,
+        field: 'fileList',
+        tableType: events.UPDATE_FILE,
+        renderView
+      }),
+      fileNameInput: new ErrorInput({
+        field: 'name',
+        placeholder: 'File Name',
+        parentState,
+        renderView
+      }),
+      fileSelector: new FileSelector({
+        field: 'filePath',
+        parentState
+      }),
+      priceInput: new ErrorInput({
+        araIcon: true,
+        errorMessage: 'Price cannot be negative',
+        field: 'price',
+        parentState,
+        placeholder: 'Price',
+        type: 'number',
+        renderView
+      })
+    }
+  }
 
-	update({ parentState }) {
-		this.props = { parentState }
-		// handles users who think `-0` is a funny price
-		parentState.price = Math.abs(parentState.price)
-		return true
-	}
+  update({ parentState }) {
+    this.props = { parentState }
+    // handles users who think `-0` is a funny price
+    parentState.price = Math.abs(parentState.price)
+    return true
+  }
 
-	createElement() {
-		const { children, props: { parentState } } = this
-		return html`
-			<div class="${styles.container} manageFile-fileInfo-container">
-				<div class=${styles.verticalContainer}>
-					<div class=${styles.infoTipHolder}>
-						${children.fileNameInput.render({ value: parentState.name })}
-						<div class=${styles.infoTip}>
-							<div>
-								<b>Recommended:</b> If this field is left blank, users will only
-								see the package's generic Ara ID.
-							</div>
-						</div>
-					</div>
-					<div class=${styles.infoTipHolder}>
-						${children.priceInput.render({ value: parentState.price, displayError: parentState.price < 0 })}
-						<div class=${styles.infoTip}>
-							Leave blank if you do not want to charge for this file.
-						</div>
-						<div class=${styles.araPriceHolder}>
-							<b>Ara Token Price:</b>
-							<div class=${styles.araPrice}>
-								<b>${parentState.tokenPrice} Ara</b>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="${styles.fileTable} manageFile-fileTable">
-					${children.fileTable.render()}
-				</div>
-			</div>
-		`
-	}
+  createElement() {
+    const { children, props: { parentState } } = this
+    return html`
+      <div class="${styles.container} manageFile-fileInfo-container">
+        <div class=${styles.verticalContainer}>
+          <div class=${styles.infoTipHolder}>
+            ${children.fileNameInput.render({ value: parentState.name })}
+            <div class=${styles.infoTip}>
+              <div>
+                <b>Recommended:</b> If this field is left blank, users will only
+                see the package's generic Ara ID.
+              </div>
+            </div>
+          </div>
+          <div class=${styles.infoTipHolder}>
+            ${children.priceInput.render({ value: parentState.price, displayError: parentState.price < 0 })}
+            <div class=${styles.infoTip}>
+              Leave blank if you do not want to charge for this file.
+            </div>
+            <div class=${styles.araPriceHolder}>
+              <b>Ara Token Price:</b>
+              <div class=${styles.araPrice}>
+                <b>${parentState.tokenPrice} Ara</b>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="${styles.fileTable} manageFile-fileTable">
+          ${children.fileTable.render()}
+        </div>
+      </div>
+    `
+  }
 }
 
 module.exports = FileInfo
