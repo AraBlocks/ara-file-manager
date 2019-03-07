219e301e58f6f788703b9cc65ce5725ec43ac4c3
Mark Romano <markgromano31@gmail.com>
2019-03-07 13:57:10 -0500

    feat(scripts/): programatically append version to .app and .exe

--- scripts/windows-dev-build.sh
+++ scripts/windows-dev-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo 'Packaging dev build'
 
-electron-packager . \
+./node_modules/.bin/electron-packager . \
 --overwrite \
 --platform=win32 \
 --arch=x64 \
 --icon=./build/icons/windows/ara_dev.ico \
 --prune=true \
 --out=release-builds \
---protocol=ara \
+--protocol=ara
