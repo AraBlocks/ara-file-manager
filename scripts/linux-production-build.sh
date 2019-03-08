38ed2b7673f5ff7cb4fd8e685bd72b306926fc3a
Madeline Cameron <github@maddie.today>
2019-03-08 18:33:07 -0500

    chore(scripts): Add RPM + flatpak generation

--- scripts/linux-production-build.sh
+++ scripts/linux-production-build.sh
@@ -8,10 +8,12 @@ electron-packager $(pwd) \
 --arch=x64 \
 --icon=./build/icons/mac/ara.icns \
 --prune=true \
---out=$(pwd)/build-releases
+--out=$(pwd)/release-builds
 
-electron-installer-debian --src $(pwd)/build-releases/ara-file-manager-linux-x64 --dest $(pwd)/build-releases --arch amd64
+electron-installer-flatpak --src $(pwd)/release-builds/ara-file-manager-linux-x64 --dest $(pwd)/build-releases --arch amd64
+electron-installer-redhat --src $(pwd)/release-builds/ara-file-manager-linux-x64 --dest $(pwd)/build-releases --arch amd64
+electron-installer-debian --src $(pwd)/release-builds/ara-file-manager-linux-x64 --dest $(pwd)/build-releases --arch amd64
 sudo docker build . -t afm
-sudo docker run -it --rm -v $(pwd)/build-releases/ara-file-manager-linux-x64:/snap/app afm --src=.
+sudo docker run -it --rm -v $(pwd)/release-builds/ara-file-manager-linux-x64:/snap/app afm --src=.
 
-mv $(pwd)/build-releases/ara-file-manager-linux-x64/*.snap $(pwd)/build-releases
+mv $(pwd)/release-builds/ara-file-manager-linux-x64/*.snap $(pwd)/build-releases
