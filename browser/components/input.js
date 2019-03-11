0f04f0d36bf8d6226054ea9d1b9189462a943a6f
Mark Romano <markgromano31@gmail.com>
2019-03-11 18:41:25 -0400

    refactor(Input | ErrorInput): simplified API

--- browser/components/input.js
+++ browser/components/input.js
@@ -8,106 +8,60 @@ class Input extends Nanocomponent {
     araIcon = false,
     cssClass = {},
     disabled = false,
-    embeddedButton = {},
-    field = '',
     onchange = () => {},
-    parentState = {},
+    oninput = () => {},
     placeholder = '',
     renderView = () => {},
     type = 'text',
-    step
+    step,
+    value = ''
   }) {
     super()
     this.props = {
       araIcon,
       cssClass,
       disabled,
-      embeddedButton,
-      field,
       onchange,
-      parentState,
+      oninput,
       placeholder,
       renderView,
       type,
-      step
+      step,
+      value
     }
-    this.state = {
-      requiredIndicator: false,
-      selection: this.props.embeddedButton.optionList ? this.props.embeddedButton.optionList[0] : '',
-      value: parentState[field]
-    }
+
     this.oninput = this.oninput.bind(this)
   }
 
-  oninput(e) {
-    const { props, state } = this
-    state.value = e.target.value
-    props.parentState[props.field] = state.value
+  oninput({ target }) {
+    const { props } = this
+    props.oninput(target.value)
     props.renderView()
   }
 
-  select(e) {
-    const { props, state } = this
-    state.selection = e.target.value
-    props.parentState[props.embeddedButton.field] = state.selection
-  }
-
-  update({ disabled , requiredIndicator = false, value = null } = {}) {
-    const { state, props } = this
-    state.value = value === null ? state.value : value
-    state.requiredIndicator = state.requiredIndicator !== requiredIndicator
-     ? requiredIndicator
-     : state.requiredIndicator
-    props.disabled = disabled !== undefined ? disabled : props.disabled
+  update(newProps) {
+    Object.assign(this.props, newProps)
     return true
   }
 
   createElement() {
-    const {
-      oninput,
-      props,
-      select,
-      state
-    } = this
+    const { oninput, props } = this
     return (html`
-      <div class="${styles.container({ disabled: props.disabled, required: state.required })} input-container">
+      <div class="${styles.container({ disabled: props.disabled, required: props.required })} input-container">
         <input class="
-          ${styles[props.cssClass.name || 'standard'](props.cssClass.opts || {})} input-dynamicClass
-          ${state.requiredIndicator ? styles.requiredIndicator : null} input-requiredIndicator
-          ${props.araIcon ? styles.icon + ' input-icon' : null}"
+          ${styles[props.cssClass.name || 'standard'](props.cssClass.opts)} input-dynamicClass
+          ${props.requiredIndicator && styles.requiredIndicator} input-requiredIndicator
+          ${props.araIcon && `${styles.icon} input-icon`}"
           onchange="${props.onchange}"
           oninput="${oninput}"
           placeholder="${props.placeholder}"
-          value="${state.value}"
+          value="${props.value}"
           type="${props.type}"
           step="${props.step}"
           disabled="${props.disabled}"
         >
-        ${generateButton()}
       </div>
     `)
-
-    function generateButton() {
-      let button = html``
-      const embeddedButton = props.embeddedButton
-      if (embeddedButton.option === 'button') {
-        button = html`
-          <button
-            class="${styles.button} input-button"
-            onclick="${embeddedButton.onclick}"
-          >
-            ${embeddedButton.children}
-          </button>`
-      } else if (embeddedButton.option === 'selection' && embeddedButton.optionList != null) {
-        props.parentState[embeddedButton.field] = embeddedButton.optionList[0]
-        button = html`
-          <select class="${styles.selection} input-selection" onchange="${select}">
-            ${embeddedButton.optionList.map(option => html`<option value="${option}">${option}</option>`)}
-          </select>
-        `
-      }
-      return button
-    }
   }
 }
 
