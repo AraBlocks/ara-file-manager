e75d1f8e03d5d212e5a5a69efd27145f0276bd60
Mark Romano <markgromano31@gmail.com>
2019-04-13 18:02:03 +0800

    test(checkbox): :check:

--- browser/components/checkbox.js
+++ browser/components/checkbox.js
@@ -1,32 +1,39 @@
-const styles = require('./styles/checkbox')
 const html = require('nanohtml')
 const Nanocomponent = require('nanocomponent')
 
+const styles = require('./styles/checkbox')
+
 class Checkbox extends Nanocomponent {
 	constructor({
+		checked = false,
 		cssClass = { opts: {} },
 		disabled = false,
 		onCheck = () => {}
-	}) {
+	} = {}) {
 		super()
-		this.props = { cssClass, disabled, onCheck }
+		this.props = {
+			checked,
+			cssClass,
+			disabled,
+			onCheck
+		}
 	}
 
-	update(newProps) {
+	update(newProps = {}) {
 		Object.assign(this.props, newProps)
 		return true
 	}
 
 	createElement() {
 		const { props } = this
-		return html`
+		return (html`
 			<div
-				class="${styles.container({ ...props, ...props.cssClass})} checkbox-container"
+				class="${styles.container({ ...props, ...props.cssClass })} checkbox-container"
 				onclick=${props.disabled ? () => {} : props.onCheck}
 			>
 				${props.checked ? '✓' : null}
 			</div>
-		`
+		`)
 	}
 }
 module.exports = Checkbox
