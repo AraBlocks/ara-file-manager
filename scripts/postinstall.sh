d1fa12220e2bd045ad6bc4002720db2f09f49062
Eric Jiang <eric@littlstar.com>
2019-08-16 14:31:37 -0400

    chore(): specify dcdn version

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove faulty utp-native
-# rm -r ./node_modules/@hyperswarm/network/node_modules/utp-native
+rm -r ./node_modules/@hyperswarm/network/node_modules/utp-native
 
 # Remove contracts
 # rm -r ./node_modules/ara-contracts/contracts
