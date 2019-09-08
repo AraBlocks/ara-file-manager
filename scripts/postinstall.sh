bca224fa6c495e5d5b0d4a1dbeac047208021d25
Kevin Faaborg <kevin@ara.one>
2019-09-08 15:16:00 -0600

    added echo notes around postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo 'postinstall.sh ---- start'
+echo ''
 
 # Remove faulty utp-native
 rm -r ./node_modules/@hyperswarm/network/node_modules/utp-native
+#TODO make a note about this, is it still necessary?
 
 # Remove contracts
 # rm -r ./node_modules/ara-contracts/contracts
+#TODO this is commented out, make a note about it or just get rid of it entirely
+
+echo 'postinstall.sh ---- done'
+echo ''
