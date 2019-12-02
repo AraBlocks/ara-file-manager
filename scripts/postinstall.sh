ed0b8e45db30d58dbc41134eaf136bbe46af25bb
Kevin Faaborg <kevin@ara.one>
2019-12-02 16:00:06 -0700

    uninstall scripts

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,14 +8,13 @@ ls ./node_modules/@hyperswarm/network/node_modules/utp-native
 rm -r ./node_modules/@hyperswarm/network/node_modules/utp-native
 echo 'ls utp-native after rm:'
 ls ./node_modules/@hyperswarm/network/node_modules/utp-native
-#TODO make a note about this, is it still necessary?
-#     added ls before and after to see if it is even deleting anything
-#TODO also, electron-builder may install-app-deps without needing to tell it to in postinstall
-#     so you may not need any of this anymore
+#TODO this does delete something, and it is necessary
+#     update and unify our use of UTP so we can get rid of this extra step
 
 # Remove contracts
 # rm -r ./node_modules/ara-contracts/contracts
-#TODO this is commented out, make a note about it or just get rid of it entirely
+#TODO this was already commented out
+#     make a note about it or just get rid of it entirely
 
 echo ''
 echo 'postinstall.sh ---- done, but more happens asynchronously'
