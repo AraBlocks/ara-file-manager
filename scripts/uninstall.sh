f43984151331a10ed2473c8fe83681d08d44588e
Kevin Faaborg <kevin@ara.one>
2019-12-03 13:05:06 -0700

    unified uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,12 +6,27 @@ echo ''
 
 #TODO just ls for now
 
-ls "/Applications/Ara File Manager.app"
+echo '(1)'
+ls /Applications/Ara\ File\ Manager.app
+echo '(2)'
 ls ~/.ara
+echo '(3)'
 ls ~/.ararc
-ls "~/Library/Application Support/Ara File Manager"
+echo '(4)'
+ls ~/AppData/Local/ara-file-manager
+echo '(5)'
+ls ~/AppData/Local/ara-updater
+echo '(6)'
+ls ~/AppData/Local/Programs/ara
+echo '(7)'
+ls ~/AppData/Roaming/Ara\ File\ Manager
+echo '(8)'
+ls ~/Library/Application\ Support/Ara\ File\ Manager
+echo '(9)'
 ls ~/Library/Preferences/com.ara.one.araFileManager.helper.plist
+echo '(10)'
 ls ~/Library/Preferences/com.ara.one.araFileManager.plist
+echo '(done)'
 
 echo ''
 echo 'Now you can reinstall the app as though this computer has never seen it before'
