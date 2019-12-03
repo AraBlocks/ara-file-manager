5a71a443ae0f92720d2ef5c4057663fdfb38a036
Kevin Faaborg <kevin@ara.one>
2019-12-03 13:14:25 -0700

    uninstall armed

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
-echo 'uninstall-mac.sh ---- start'
+echo 'uninstall.sh ---- start'
 echo ''
-echo 'Deleting all the paths where this or previous versions of the app may have left files'
+echo 'Deleting all the paths where the app may have left files'
+echo 'Includes program, settings, and user document files, this and previous versions, and macOS, Windows, and Linux'
 echo ''
 
-#TODO just ls for now
+rm -rf /Applications/Ara\ File\ Manager.app
+rm -rf ~/.ara
+rm -rf ~/.ararc
+rm -rf ~/AppData/Local/ara-file-manager
+rm -rf ~/AppData/Local/ara-updater
+rm -rf ~/AppData/Local/Programs/ara
+rm -rf ~/AppData/Roaming/Ara\ File\ Manager
+rm -rf ~/Library/Application\ Support/Ara\ File\ Manager
+rm -rf ~/Library/Preferences/com.ara.one.araFileManager.helper.plist
+rm -rf ~/Library/Preferences/com.ara.one.araFileManager.plist
 
-echo '(1)'
 ls /Applications/Ara\ File\ Manager.app
-echo '(2)'
 ls ~/.ara
-echo '(3)'
 ls ~/.ararc
-echo '(4)'
 ls ~/AppData/Local/ara-file-manager
-echo '(5)'
 ls ~/AppData/Local/ara-updater
-echo '(6)'
 ls ~/AppData/Local/Programs/ara
-echo '(7)'
 ls ~/AppData/Roaming/Ara\ File\ Manager
-echo '(8)'
 ls ~/Library/Application\ Support/Ara\ File\ Manager
-echo '(9)'
 ls ~/Library/Preferences/com.ara.one.araFileManager.helper.plist
-echo '(10)'
 ls ~/Library/Preferences/com.ara.one.araFileManager.plist
-echo '(done)'
 
 echo ''
 echo 'Now you can reinstall the app as though this computer has never seen it before'
 echo ''
-echo 'uninstall-mac.sh ---- done'
+echo 'uninstall.sh ---- done'
 echo ''
