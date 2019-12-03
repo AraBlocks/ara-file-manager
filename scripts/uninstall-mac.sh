0305ef5d977ea7a2a6e7ebd21c076063f87fe8d0
Kevin Faaborg <kevin@ara.one>
2019-12-03 12:50:25 -0700

    back to windows

--- scripts/uninstall-mac.sh
+++ scripts/uninstall-mac.sh
@@ -7,12 +7,11 @@ echo ''
 #TODO just ls for now
 
 ls "/Applications/Ara File Manager.app"
-ls "/Applications/Tortuga.app"
-ls "~/.ara"
-ls "~/.ararc"
+ls ~/.ara
+ls ~/.ararc
 ls "~/Library/Application Support/Ara File Manager"
-ls "~/Library/Preferences/com.ara.one.araFileManager.helper.plist"
-ls "~/Library/Preferences/com.ara.one.araFileManager.plist"
+ls ~/Library/Preferences/com.ara.one.araFileManager.helper.plist
+ls ~/Library/Preferences/com.ara.one.araFileManager.plist
 
 echo ''
 echo 'Now you can reinstall the app as though this computer has never seen it before'
