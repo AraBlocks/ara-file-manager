18e54fa0cd50aaeb536c0e2df252fe9c505e7848
Kevin Faaborg <kevin@ara.one>
2019-12-16 13:27:37 -0500

    activate event

--- boot/index.js
+++ boot/index.js
@@ -39,6 +39,9 @@ app.on('ready', () => {
 //Prevents app from closing when all windows are closed
 app.on('window-all-closed', () => { })
 
+//"launching the application for the first time, attempting to re-launch the application when it's already running, or clicking on the application's dock or taskbar icon"
+app.on('activate', () => openWindow('filemanager'))
+
 // For Deep Linking
 app.setAsDefaultProtocolClient('ara')
 app.on('open-url', handleDeepLink)
