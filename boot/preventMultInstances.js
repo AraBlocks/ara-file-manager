8d1972c447631b2a0a16026f68c94d2ebfe642a3
Brandon Plaster <brandon.a.plaster@gmail.com>
2021-03-27 16:46:11 -0600

    chore: update to node 12

--- boot/preventMultInstances.js
+++ boot/preventMultInstances.js
@@ -1,11 +1,13 @@
 const { app } = require('electron')
 const { openDeepLinking } = require('electron-window-manager')
 
-const shouldQuit = app.makeSingleInstance((argv, workingDirectory) => {
-  if (process.platform === 'win32') {
-    const arg = argv[1]
-    if (arg && arg.includes('ara://')) { openDeepLinking(arg) }
-  }
-})
+if (!app.requestSingleInstanceLock()) { return app.quit() }
+else {
+	app.on('second-instance', (event, argv, workingDirectory) => {
+		if (process.platform === 'win32') {
+			const arg = argv[1]
+			if (arg && arg.includes('ara://')) { openDeepLinking(arg) }
+		}
+	})
+}
 
-if (shouldQuit) { return app.quit() }
